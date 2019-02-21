16de3338ef5377457ed0d22c46631709e221942e
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2019-02-21 13:06:04 +0100

    PFP-5955 legger til hentedato i hentvirksomhetforjuridisk

--- integrasjon/organisasjon-klient/src/test/java/no/nav/vedtak/felles/integrasjon/organisasjon/OrganisasjonConsumerImplTest.java
+++ integrasjon/organisasjon-klient/src/test/java/no/nav/vedtak/felles/integrasjon/organisasjon/OrganisasjonConsumerImplTest.java
@@ -2,21 +2,21 @@ package no.nav.vedtak.felles.integrasjon.organisasjon;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import no.nav.tjeneste.virksomhet.organisasjon.v4.binding.OrganisasjonV4;
-import no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentOrganisasjonResponse;
+import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import no.nav.vedtak.felles.integrasjon.organisasjon.OrganisasjonConsumerImpl;
+import no.nav.tjeneste.virksomhet.organisasjon.v4.binding.OrganisasjonV4;
+import no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentOrganisasjonResponse;
+import no.nav.vedtak.felles.integrasjon.organisasjon.hent.HentOrganisasjonForJuridiskRequest;
 import no.nav.vedtak.felles.integrasjon.organisasjon.hent.HentOrganisasjonRequest;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 
 public class OrganisasjonConsumerImplTest {
 
@@ -46,6 +46,22 @@ public class OrganisasjonConsumerImplTest {
 
         // Assert
         verify(port, times(1)).hentOrganisasjon(any(no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentOrganisasjonRequest.class));
+    }
+
+    @Test
+    public void skal_kalle_ws_for_å_hente_organisasjon_for_juridisk() throws Exception {
+        // Arrange
+        no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentVirksomhetsOrgnrForJuridiskOrgnrBolkResponse hentVirksomhetsOrgnrForJuridiskOrgnrBolkResponse =
+            new no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentVirksomhetsOrgnrForJuridiskOrgnrBolkResponse();
+        when(port.hentVirksomhetsOrgnrForJuridiskOrgnrBolk(any(no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentVirksomhetsOrgnrForJuridiskOrgnrBolkRequest.class))).thenReturn(hentVirksomhetsOrgnrForJuridiskOrgnrBolkResponse);
+
+        // Act
+        HentOrganisasjonForJuridiskRequest request = new HentOrganisasjonForJuridiskRequest("918450165", LocalDate.now());
+        @SuppressWarnings("unused")
+        no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentVirksomhetsOrgnrForJuridiskOrgnrBolkResponse response = consumer.hentOrganisajonerForJuridiskOrgnr(request);
+
+        // Assert
+        verify(port, times(1)).hentVirksomhetsOrgnrForJuridiskOrgnrBolk(any(no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.HentVirksomhetsOrgnrForJuridiskOrgnrBolkRequest.class));
     }
 
 }
