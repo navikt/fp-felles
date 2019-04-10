171e616dc062a23e6c1f31e9fd405a925f1e7f74
Øystein Urdahl Hardeng <oystein.urdahl.hardeng@nav.no>
2019-04-10 16:05:31 +0200

    PFP-1899 Legger til manglende migrert commit fra Stash (8500095bf2e)

---
+++ integrasjon/webservice/src/main/java/no/nav/vedtak/felles/integrasjon/felles/ws/SAMLLogoutInterceptor.java
@@ -0,0 +1,71 @@
+package no.nav.vedtak.felles.integrasjon.felles.ws;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.phase.PhaseInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SAMLLogoutInterceptor implements Interceptor<SoapMessage>,
+    PhaseInterceptor<SoapMessage> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SAMLLogoutInterceptor.class);
+    private LoginContext loginContext;
+
+    public SAMLLogoutInterceptor(LoginContext loginContext) {
+        this.loginContext = loginContext;
+    }
+
+    @Override
+    public void handleMessage(SoapMessage message) throws Fault {
+        logout();
+    }
+
+    @Override
+    public void handleFault(SoapMessage message) {
+        logout();
+    }
+
+    private void logout() {
+        try {
+            loginContext.logout();
+        } catch (LoginException e) {
+            SoapWebServiceFeil.FACTORY.feiletUtlogging(e).log(LOGGER);
+        }
+    }
+
+    @Override
+    public Set<String> getAfter() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public Set<String> getBefore() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getId() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getPhase() {
+        return Phase.POST_INVOKE;
+    }
+
+    @Override
+    public Collection<PhaseInterceptor<? extends Message>> getAdditionalInterceptors() {
+        return null;
+    }
+}
--- integrasjon/webservice/src/main/java/no/nav/vedtak/felles/integrasjon/felles/ws/SoapWebServiceFeil.java
+++ integrasjon/webservice/src/main/java/no/nav/vedtak/felles/integrasjon/felles/ws/SoapWebServiceFeil.java
@@ -1,5 +1,6 @@
 package no.nav.vedtak.felles.integrasjon.felles.ws;
 
+import javax.security.auth.login.LoginException;
 import javax.xml.ws.WebServiceException;
 
 import no.nav.vedtak.feil.Feil;
@@ -7,10 +8,14 @@ import no.nav.vedtak.feil.FeilFactory;
 import no.nav.vedtak.feil.LogLevel;
 import no.nav.vedtak.feil.deklarasjon.DeklarerteFeil;
 import no.nav.vedtak.feil.deklarasjon.IntegrasjonFeil;
+import no.nav.vedtak.feil.deklarasjon.TekniskFeil;
 
 public interface SoapWebServiceFeil extends DeklarerteFeil {
     SoapWebServiceFeil FACTORY = FeilFactory.create(SoapWebServiceFeil.class);
 
     @IntegrasjonFeil(feilkode = "FP-942048", feilmelding = "SOAP tjenesten [ %s ] returnerte en SOAP Fault: %s", logLevel = LogLevel.WARN)
     Feil soapFaultIwebserviceKall(String webservice, WebServiceException soapException);
+
+    @TekniskFeil(feilkode = "F-668217", feilmelding = "Feilet utlogging.", logLevel = LogLevel.WARN)
+    Feil feiletUtlogging(LoginException e);
 }
