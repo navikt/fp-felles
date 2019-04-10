73e172591be8c9144ab8572b79a6fe8875e7fbf6
Øystein Urdahl Hardeng <oystein.urdahl.hardeng@nav.no>
2019-04-10 16:09:34 +0200

    PFP-1899 Bedret logging

--- felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/jaspic/JaspicFeil.java
+++ felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/jaspic/JaspicFeil.java
@@ -12,6 +12,6 @@ import java.util.Set;
 public interface JaspicFeil extends DeklarerteFeil {
     JaspicFeil FACTORY = FeilFactory.create(JaspicFeil.class);
 
-    @TekniskFeil(feilkode = "F-498054", feilmelding = "Denne SKAL rapporteres som en bug hvis den dukker opp. Tråden inneholdt allerede et Subject med følgende principals: %s. Sletter det før autentisering fortsetter.", logLevel = LogLevel.INFO)
-    Feil eksisterendeSubject(Set<Principal> principals);
+    @TekniskFeil(feilkode = "F-498054", feilmelding = "Denne SKAL rapporteres som en bug hvis den dukker opp. Tråden inneholdt allerede et Subject med følgende principals {%s} og PublicCredentials klasser {%s}. Sletter det før autentisering fortsetter.", logLevel = LogLevel.INFO)
+    Feil eksisterendeSubject(Set<Principal> principals, Set<String> credidentialClasses);
 }
