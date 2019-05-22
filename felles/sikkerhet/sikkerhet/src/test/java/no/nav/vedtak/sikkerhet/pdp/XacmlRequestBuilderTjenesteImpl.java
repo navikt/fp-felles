58b7e808448a0a2de75cb3ab306f35085401f184
Marius Bækken Glittum <ekunamatata@gmail.com>
2019-05-22 10:23:33 +0200

    Oppdaterer eksempel (#56)
    - Fjerner bruk av index

--- felles/sikkerhet/sikkerhet/src/test/java/no/nav/vedtak/sikkerhet/pdp/XacmlRequestBuilderTjenesteImpl.java
+++ felles/sikkerhet/sikkerhet/src/test/java/no/nav/vedtak/sikkerhet/pdp/XacmlRequestBuilderTjenesteImpl.java
@@ -1,21 +1,19 @@
 package no.nav.vedtak.sikkerhet.pdp;
 
-import static no.nav.abac.xacml.NavAttributter.RESOURCE_ARKIV_GSAK_SAKSID;
-import static no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE;
-import static no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_PERSON_FNR;
 import static no.nav.abac.xacml.NavAttributter.RESOURCE_FORELDREPENGER_SAK_AKSJONSPUNKT_TYPE;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.Dependent;
 
-import no.nav.abac.foreldrepenger.xacml.ForeldrepengerAttributter;
 import no.nav.abac.xacml.NavAttributter;
 import no.nav.abac.xacml.StandardAttributter;
 import no.nav.vedtak.sikkerhet.abac.PdpRequest;
 import no.nav.vedtak.sikkerhet.pdp.xacml.XacmlAttributeSet;
 import no.nav.vedtak.sikkerhet.pdp.xacml.XacmlRequestBuilder;
+import no.nav.vedtak.util.Tuple;
 
 @Dependent
 public class XacmlRequestBuilderTjenesteImpl implements XacmlRequestBuilderTjeneste {
@@ -31,47 +29,43 @@ public class XacmlRequestBuilderTjenesteImpl implements XacmlRequestBuilderTjene
         actionAttributeSet.addAttribute(StandardAttributter.ACTION_ID, pdpRequest.getString(StandardAttributter.ACTION_ID));
         xacmlBuilder.addActionAttributeSet(actionAttributeSet);
 
-        int antall = antallResources(pdpRequest);
-        for (int i = 0; i < antall; i++) {
-            XacmlAttributeSet resourceAttributeSet = byggXacmlResourceAttrSet(pdpRequest, i);
-            xacmlBuilder.addResourceAttributeSet(resourceAttributeSet);
+        List<Tuple<String, String>> identer = hentIdenter(pdpRequest, NavAttributter.RESOURCE_FELLES_PERSON_FNR, NavAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE);
+
+        if (identer.isEmpty()) {
+            populerResources(xacmlBuilder, pdpRequest, null);
+        } else {
+            for (Tuple<String, String> ident : identer) {
+                populerResources(xacmlBuilder, pdpRequest, ident);
+            }
         }
 
         return xacmlBuilder;
     }
 
-    private int antallResources(PdpRequest pdpRequest) {
-        return Math.max(1, antallIdenter(pdpRequest)) * Math.max(1, antallAksjonspunktTyper(pdpRequest));
+    private void populerResources(XacmlRequestBuilder xacmlBuilder, PdpRequest pdpRequest, Tuple<String, String> ident) {
+        List<String> aksjonspunktTyper = pdpRequest.getListOfString(RESOURCE_FORELDREPENGER_SAK_AKSJONSPUNKT_TYPE);
+        if (aksjonspunktTyper.isEmpty()) {
+            xacmlBuilder.addResourceAttributeSet(byggRessursAttributter(pdpRequest, ident, null));
+        } else {
+            for (String aksjonspunktType : aksjonspunktTyper) {
+                xacmlBuilder.addResourceAttributeSet(byggRessursAttributter(pdpRequest, ident, aksjonspunktType));
+            }
+        }
     }
 
-    private int antallIdenter(PdpRequest pdpRequest) {
-        return pdpRequest.getAntall(RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE) + pdpRequest.getAntall(RESOURCE_FELLES_PERSON_FNR);
-    }
-
-    private int antallAksjonspunktTyper(PdpRequest pdpRequest) {
-        return pdpRequest.getAntall(RESOURCE_FORELDREPENGER_SAK_AKSJONSPUNKT_TYPE);
-    }
-
-    XacmlAttributeSet byggXacmlResourceAttrSet(PdpRequest pdpRequest, int index) {
-
+    private XacmlAttributeSet byggRessursAttributter(PdpRequest pdpRequest, Tuple<String, String> ident, String aksjonsounktType) {
         XacmlAttributeSet resourceAttributeSet = new XacmlAttributeSet();
         resourceAttributeSet.addAttribute(NavAttributter.RESOURCE_FELLES_DOMENE, pdpRequest.getString(NavAttributter.RESOURCE_FELLES_DOMENE));
         resourceAttributeSet.addAttribute(NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE, pdpRequest.getString(NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE));
-
-        int antallFnrPåRequest = pdpRequest.getAntall(RESOURCE_FELLES_PERSON_FNR);
-        if (index < antallFnrPåRequest) {
-            setOptionalListValueinAttributeSet(resourceAttributeSet, pdpRequest, RESOURCE_FELLES_PERSON_FNR, index % antallFnrPåRequest);
-        } else {
-            int kalkulertIndex = (index - antallFnrPåRequest) % Math.max(pdpRequest.getAntall(RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE), 1);
-            setOptionalListValueinAttributeSet(resourceAttributeSet, pdpRequest, RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE, kalkulertIndex);
-        }
-        setOptionalListValueinAttributeSet(resourceAttributeSet, pdpRequest, RESOURCE_FORELDREPENGER_SAK_AKSJONSPUNKT_TYPE, (index / Math.max(antallIdenter(pdpRequest), 1)));
         setOptionalValueinAttributeSet(resourceAttributeSet, pdpRequest, NavAttributter.RESOURCE_FORELDREPENGER_SAK_SAKSSTATUS);
         setOptionalValueinAttributeSet(resourceAttributeSet, pdpRequest, NavAttributter.RESOURCE_FORELDREPENGER_SAK_BEHANDLINGSSTATUS);
         setOptionalValueinAttributeSet(resourceAttributeSet, pdpRequest, NavAttributter.RESOURCE_FORELDREPENGER_SAK_ANSVARLIG_SAKSBEHANDLER);
-        setOptionalListValueinAttributeSet(resourceAttributeSet, pdpRequest, RESOURCE_ARKIV_GSAK_SAKSID, index);
-        int kalkulertOppgaveIndex = index % Math.max(pdpRequest.getAntall(ForeldrepengerAttributter.FORELDREPENGER_OPPGAVESTYRING_AVDELINGSENHET), 1);
-        setOptionalListValueinAttributeSet(resourceAttributeSet, pdpRequest, ForeldrepengerAttributter.FORELDREPENGER_OPPGAVESTYRING_AVDELINGSENHET, kalkulertOppgaveIndex);
+        if (ident != null) {
+            resourceAttributeSet.addAttribute(ident.getElement1(), ident.getElement2());
+        }
+        if (aksjonsounktType != null) {
+            resourceAttributeSet.addAttribute(NavAttributter.RESOURCE_FORELDREPENGER_SAK_AKSJONSPUNKT_TYPE, aksjonsounktType);
+        }
 
         return resourceAttributeSet;
     }
@@ -80,10 +74,11 @@ public class XacmlRequestBuilderTjenesteImpl implements XacmlRequestBuilderTjene
         pdpRequest.getOptional(key).ifPresent(s -> resourceAttributeSet.addAttribute(key, s));
     }
 
-    private void setOptionalListValueinAttributeSet(XacmlAttributeSet resourceAttributeSet, PdpRequest pdpRequest, String key, int index) {
-        List<String> list = pdpRequest.getListOfString(key);
-        if (list.size() >= index + 1) {
-            Optional.ofNullable(list.get(index)).ifPresent(s -> resourceAttributeSet.addAttribute(key, s));
+    private List<Tuple<String, String>> hentIdenter(PdpRequest pdpRequest, String... identNøkler) {
+        List<Tuple<String, String>> identer = new ArrayList<>();
+        for (String key : identNøkler) {
+            identer.addAll(pdpRequest.getListOfString(key).stream().map(it -> new Tuple<>(key, it)).collect(Collectors.toList()));
         }
+        return identer;
     }
 }
