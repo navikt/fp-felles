423c87f893b522b165bf1b6effc30809f16d50e5
tendestad <45484434+tendestad@users.noreply.github.com>
2019-05-23 15:55:30 +0200

    PFP-7955 Kan sende navn på kvitteringskøen ved iverksettelse mot OS (#57)

--- integrasjon/okonomistotte-jms/src/main/java/no/nav/vedtak/felles/integrasjon/okonomistottejms/producer/ØkonomioppdragJmsProducerKonfig.java
+++ integrasjon/okonomistotte-jms/src/main/java/no/nav/vedtak/felles/integrasjon/okonomistottejms/producer/ØkonomioppdragJmsProducerKonfig.java
@@ -1,7 +1,10 @@
 package no.nav.vedtak.felles.integrasjon.okonomistottejms.producer;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import no.finn.unleash.Unleash;
 import no.nav.vedtak.felles.integrasjon.jms.BaseJmsKonfig;
 
 @Named("økonomioppdragjmsproducerkonfig")
@@ -11,8 +14,11 @@ public class ØkonomioppdragJmsProducerKonfig extends BaseJmsKonfig {
     public static final String JNDI_QUEUE = "jms/QueueFpsakOkonomiOppdragSend";
 
     private static final String UT_QUEUE_PREFIX = "fpsak_okonomi_oppdrag_send";
+    private static final String KVITTERING_QUEUE_PREFIX = "fpsak_okonomi_oppdrag_mottak";
 
-    public ØkonomioppdragJmsProducerKonfig() {
-        super(UT_QUEUE_PREFIX);
+    @Inject
+    public ØkonomioppdragJmsProducerKonfig(Unleash unleash) {
+        super(UT_QUEUE_PREFIX, unleash.isEnabled("fpsak.send.kvitteringskoenavn") ? KVITTERING_QUEUE_PREFIX : null);
     }
 }
+
