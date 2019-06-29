d9f2bac821810922b91774feabfb41da188845ad
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-06-29 09:35:33 +0200

    FIX gjentar PDP kall ved IOException første gang (#70)

--- felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/pdp/PdpConsumerImpl.java
+++ felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/pdp/PdpConsumerImpl.java
@@ -12,8 +12,11 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonValue;
 
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
@@ -25,13 +28,18 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,7 +55,7 @@ public class PdpConsumerImpl implements PdpConsumer {
     private static final String PDP_ENDPOINT_URL_KEY = "abac.pdp.endpoint.url";
     private static final String SYSTEMBRUKER_USERNAME = "systembruker.username";
     private static final String SYSTEMBRUKER_PASSWORD = "systembruker.password"; // NOSONAR
-    private static final int MAX_TOTAL_CONNECTIONS = 20;
+    private static final int MAX_TOTAL_CONNECTIONS_PER_ROUTE = 20;
     private static final String MEDIA_TYPE = "application/xacml+json";
     private static final Logger LOG = LoggerFactory.getLogger(PdpConsumerImpl.class);
 
@@ -59,10 +67,11 @@ public class PdpConsumerImpl implements PdpConsumer {
     private Tuple<CloseableHttpClient, AuthCache> activeConfiguration;
 
     PdpConsumerImpl() {
-    } //CDI
+    } // CDI
 
     @Inject
-    public PdpConsumerImpl(@KonfigVerdi(PDP_ENDPOINT_URL_KEY) String pdpUrl, @KonfigVerdi(SYSTEMBRUKER_USERNAME) String brukernavn, @KonfigVerdi(SYSTEMBRUKER_PASSWORD) String passord) {
+    public PdpConsumerImpl(@KonfigVerdi(PDP_ENDPOINT_URL_KEY) String pdpUrl, @KonfigVerdi(SYSTEMBRUKER_USERNAME) String brukernavn,
+                           @KonfigVerdi(SYSTEMBRUKER_PASSWORD) String passord) {
         this.pdpUrl = pdpUrl;
         this.brukernavn = brukernavn;
         this.passord = passord;
@@ -70,11 +79,11 @@ public class PdpConsumerImpl implements PdpConsumer {
         activeConfiguration = buildClient();
     }
 
+    @SuppressWarnings("resource")
     private Tuple<CloseableHttpClient, AuthCache> buildClient() {
-        @SuppressWarnings("resource")
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
-        cm.setMaxTotal(MAX_TOTAL_CONNECTIONS);
-        cm.setDefaultMaxPerRoute(MAX_TOTAL_CONNECTIONS);
+        cm.setDefaultMaxPerRoute(MAX_TOTAL_CONNECTIONS_PER_ROUTE);
+        cm.setMaxTotal(3 * MAX_TOTAL_CONNECTIONS_PER_ROUTE); // i tilfelle redirects
 
         CredentialsProvider credsProvider = new BasicCredentialsProvider();
         credsProvider.setCredentials(new AuthScope(target.getHostName(), target.getPort()), new UsernamePasswordCredentials(brukernavn, passord));
@@ -88,6 +97,8 @@ public class PdpConsumerImpl implements PdpConsumer {
             .setConnectionManager(cm)
             .setDefaultCredentialsProvider(credsProvider)
             .setDefaultRequestConfig(requestConfig)
+            .setRetryHandler(new StandardHttpRequestRetryHandler()) // idempotente HTTP kall kan gjentas
+            .setKeepAliveStrategy(createKeepAliveStrategy(30)) // keep max 30 sek keepalive på connections hvis ikke angitt av server
             .build();
 
         AuthCache authCache = new BasicAuthCache();
@@ -105,6 +116,7 @@ public class PdpConsumerImpl implements PdpConsumer {
         HttpPost post = new HttpPost(pdpUrl);
         post.setHeader("Content-type", MEDIA_TYPE);
         post.setEntity(new StringEntity(request.toString(), Charset.forName("UTF-8")));
+
         LOG.trace("PDP-request: {}", request);
 
         Tuple<CloseableHttpClient, AuthCache> active = activeConfiguration;
@@ -117,6 +129,8 @@ public class PdpConsumerImpl implements PdpConsumer {
         if (HttpStatus.SC_UNAUTHORIZED == statusCode) {
             synchronized (this) {
                 if (active == activeConfiguration) {
+                    // reset client bør sjelden skje, skal ikke havne i situasjonen at vi spør etter ting de ikke er authorized til, men 
+                    // det skjer at PDP server f.eks. er resatt og eneste vi kan gjøre er å resette vår egen tilstand.
                     activeConfiguration = buildClient();
                     PdpFeil.FACTORY.reinstansiertHttpClient().log(LOG);
                 }
@@ -136,28 +150,41 @@ public class PdpConsumerImpl implements PdpConsumer {
 
     }
 
+    @SuppressWarnings("resource")
     private Tuple<StatusLine, JsonObject> call(Tuple<CloseableHttpClient, AuthCache> active, HttpPost post) {
         final CloseableHttpClient client = active.getElement1();
         final AuthCache authCache = active.getElement2();
 
-        HttpClientContext context = HttpClientContext.create();
-        context.setAuthCache(authCache);
-        try (CloseableHttpResponse response = client.execute(target, post, context)) {
-            final StatusLine statusLine = response.getStatusLine();
-            if (HttpStatus.SC_OK == statusLine.getStatusCode()) {
-                final HttpEntity entity = response.getEntity();
-                try (JsonReader reader = Json.createReader(entity.getContent())) {
-                    JsonObject jsonResponse = reader.readObject();
-                    LOG.trace("PDP-response: {}", jsonResponse);
-                    return new Tuple<>(statusLine, jsonResponse);
+        int retries = 2;
+        StatusLine statusLine = null;
+        while (--retries >= 0) {
+            HttpClientContext context = HttpClientContext.create();
+            context.setAuthCache(authCache);
+            try (CloseableHttpResponse response = client.execute(target, post, context)) {
+                statusLine = response.getStatusLine();
+                if (HttpStatus.SC_OK == statusLine.getStatusCode()) {
+                    final HttpEntity entity = response.getEntity();
+                    try (JsonReader reader = Json.createReader(entity.getContent())) {
+                        JsonObject jsonResponse = reader.readObject();
+                        LOG.trace("PDP-response: {}", jsonResponse);
+                        return new Tuple<>(statusLine, jsonResponse);
+                    }
                 }
+                break;
+            } catch (IOException e) {
+                if (retries > 0) {
+                    // ved IO feil gjør et forsøk til (server kan ha stengt conn)
+                    // logg kun første gang vi treffer, kast exception andre gang
+                    LOG.trace("Fikk IOException - PDP feil, prøver en gang til", e);
+                } else {
+                    throw PdpFeil.FACTORY.ioFeil(e).toException();
+                }
+            } finally {
+                post.releaseConnection();
             }
-            return new Tuple<>(statusLine, JsonValue.EMPTY_JSON_OBJECT);
-        } catch (IOException e) {
-            throw PdpFeil.FACTORY.ioFeil(e).toException();
-        } finally {
-            post.releaseConnection();
         }
+        
+        return new Tuple<>(statusLine, JsonValue.EMPTY_JSON_OBJECT);
     }
 
     private String getSchemaAndHostFromURL(String pdpUrl) {
@@ -168,4 +195,25 @@ public class PdpConsumerImpl implements PdpConsumer {
             throw new IllegalArgumentException(e);
         }
     }
+
+    /** Sørger for å droppe og starte nye connections innimellom også om server ikke sender keepalive header. */
+    private static ConnectionKeepAliveStrategy createKeepAliveStrategy(int seconds) {
+        ConnectionKeepAliveStrategy myStrategy = new ConnectionKeepAliveStrategy() {
+            @Override
+            public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+                HeaderElementIterator it = new BasicHeaderElementIterator(response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+                while (it.hasNext()) {
+                    HeaderElement he = it.nextElement();
+                    String param = he.getName();
+                    String value = he.getValue();
+                    if (value != null && param.equalsIgnoreCase("timeout")) {
+                        return Long.parseLong(value) * 1000;
+                    }
+                }
+                return seconds * 1000;
+            }
+        };
+        return myStrategy;
+    }
+
 }
