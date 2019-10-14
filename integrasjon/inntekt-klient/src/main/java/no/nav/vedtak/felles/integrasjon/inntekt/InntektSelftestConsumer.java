756adc669e86adde6c24e5c9ebb2ba35e642fa6c
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-10-14 09:15:21 +0200

    FIX fjerner felles-integrasjon-bom (siden alle trekker inn avhengigheter de trenger). Rydder felles-integrasjon-pom slik at denne definerer alle undermoduler i dependencymanagement. Fjerner inntekt-klient

--- integrasjon/inntekt-klient/src/main/java/no/nav/vedtak/felles/integrasjon/inntekt/InntektSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.inntekt;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface InntektSelftestConsumer extends SelftestConsumer {
-}
