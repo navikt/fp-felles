a6a7e553cf95ad15eddfcf75c9b89d9aa980e036
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2019-11-13 12:52:18 +0100

    TFP-1860: Fjernet dokumentproduksjon-klient, da denne nå er lagt inn i fp-formidling, som er den eneste applikasjonen som bruker den.

--- integrasjon/dokumentproduksjon-klient/src/main/java/no/nav/vedtak/felles/integrasjon/dokument/produksjon/v2/DokumentproduksjonSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.dokument.produksjon.v2;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface DokumentproduksjonSelftestConsumer extends SelftestConsumer {
-}
