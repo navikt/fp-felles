35a292911563ceb1fb027b93e45976c307779a82
Marius Glittum <marius.glittum@nav.no>
2019-11-14 10:19:51 +0100

    Prøver å laste opp sources - fjerner duplikat i xmlsec

--- upload.sh
+++ upload.sh
@@ -6,7 +6,9 @@ function deployArtifact() {
   VERSION="$2"
   POM_FOLDER="$(echo ${POM_PATH} | sed "s/flattened//")"
   if [[ -d "${POM_FOLDER}/target/" && $(ls "${POM_FOLDER}/target/." | grep "\-${VERSION}.jar" | wc -l | xargs) -gt 0 ]]; then
-    ls "${POM_FOLDER}/target/." | grep "\-${VERSION}.jar" | xargs -L1 -I{} mvn deploy:deploy-file -q -s ./.m2/settings.xml -DrepositoryId=github -Dfile="${POM_FOLDER}/target/"{} -DpomFile="${POM_PATH}" -Durl=https://maven.pkg.github.com/navikt/fp-felles
+    toBeReplaced=".jar"
+    sourcesPostfix="-sources.jar"
+    ls "${POM_FOLDER}/target/." | grep "\-${VERSION}.jar" | xargs -L1 -I{} mvn deploy:deploy-file -q -s ./.m2/settings.xml -DrepositoryId=github -Dfile="${POM_FOLDER}/target/"{} -DsourceFile="${POM_FOLDER}/target/${{}/$toBeReplaced/$sourcesPostfix}" -DpomFile="${POM_PATH}" -Durl=https://maven.pkg.github.com/navikt/fp-felles
   else
     mvn deploy:deploy-file -q -s ./.m2/settings.xml -DrepositoryId=github -Dfile="${POM_PATH}" -DpomFile="${POM_PATH}" -Durl=https://maven.pkg.github.com/navikt/fp-felles
   fi
