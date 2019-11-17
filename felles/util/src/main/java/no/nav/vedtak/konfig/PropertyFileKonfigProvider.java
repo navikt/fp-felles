b5c1041c87fc9ce9f85114d35e4f1523ec9263f3
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-17 16:04:59 +0100

    lokal dbprovider har pri 100, må legges oss bak den

--- felles/util/src/main/java/no/nav/vedtak/konfig/PropertyFileKonfigProvider.java
+++ felles/util/src/main/java/no/nav/vedtak/konfig/PropertyFileKonfigProvider.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 @ApplicationScoped
 public class PropertyFileKonfigProvider extends PropertiesKonfigVerdiProvider {
 
+    private static final int PRIORITET = Integer.MAX_VALUE;
     private static final String LOCAL = "local";
     private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
     private static final String SUFFIX = ".properties";
@@ -22,19 +23,14 @@ public class PropertyFileKonfigProvider extends PropertiesKonfigVerdiProvider {
     private static final Logger LOG = LoggerFactory.getLogger(PropertyFileKonfigProvider.class);
 
     protected PropertyFileKonfigProvider() {
-        super(properties(PREFIX));
+        super(lesFra(PREFIX));
     }
 
-    private static Properties properties(String prefix) {
-        return properties(prefix + "-" + cluster(), properties(prefix, new Properties()));
+    private static Properties lesFra(String prefix) {
+        return lesFra(prefix + "-" + cluster(), lesFra(prefix, new Properties()));
     }
 
-    private static String cluster() {
-        return Optional.ofNullable(getenv(NAIS_CLUSTER_NAME))
-                .orElse(LOCAL);
-    }
-
-    private static Properties properties(String prefix, Properties p) {
+    private static Properties lesFra(String prefix, Properties p) {
         String navn = prefix + SUFFIX;
         try (InputStream is = PropertyFileKonfigProvider.class.getClassLoader().getResourceAsStream(navn)) {
             if (is != null) {
@@ -49,6 +45,11 @@ public class PropertyFileKonfigProvider extends PropertiesKonfigVerdiProvider {
 
     @Override
     public int getPrioritet() {
-        return 12;
+        return PRIORITET;
+    }
+
+    private static String cluster() {
+        return Optional.ofNullable(getenv(NAIS_CLUSTER_NAME))
+                .orElse(LOCAL);
     }
 }
