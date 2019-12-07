f2ba155694667a36361374a486aaf9831560b48a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-12-07 13:04:02 +0100

    aksess properties fra environment

--- felles/util/src/main/java/no/nav/vedtak/util/env/Environment.java
+++ felles/util/src/main/java/no/nav/vedtak/util/env/Environment.java
@@ -1,13 +1,31 @@
 package no.nav.vedtak.util.env;
 
+import java.net.URI;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+import no.nav.vedtak.konfig.EnvPropertiesKonfigVerdiProvider;
+import no.nav.vedtak.konfig.KonfigVerdi;
+import no.nav.vedtak.konfig.KonfigVerdi.Converter;
+import no.nav.vedtak.konfig.KonfigVerdiProvider;
+import no.nav.vedtak.konfig.PropertyFileKonfigProvider;
+import no.nav.vedtak.konfig.SystemPropertiesKonfigVerdiProvider;
+
 public final class Environment {
 
     private final Cluster cluster;
     private final Namespace namespace;
+    private final List<KonfigVerdiProvider> propertySources;
 
     private Environment(Cluster cluster, Namespace namespace) {
         this.cluster = cluster;
         this.namespace = namespace;
+        this.propertySources = List.of(
+                new SystemPropertiesKonfigVerdiProvider(),
+                new EnvPropertiesKonfigVerdiProvider(),
+                new PropertyFileKonfigProvider());
     }
 
     private static Environment of(Cluster cluster, Namespace namespace) {
@@ -36,6 +54,63 @@ public final class Environment {
 
     public String namespace() {
         return namespace.getNamespace();
+    }
+
+    public String getProperty(String key) {
+        return getProperty(key, String.class, null);
+    }
+
+    public String getProperty(String key, String defaultVerdi) {
+        return getProperty(key, String.class, defaultVerdi);
+    }
+
+    public <T> T getProperty(String key, Class<T> targetType) {
+        return getProperty(key, targetType, null);
+    }
+
+    public <T> T getProperty(String key, Class<T> targetType, T defaultVerdi) {
+        var converter = converterFor(targetType);
+        if (converter == null && !targetType.equals(String.class)) {
+            throw new IllegalArgumentException("Konvertering til " + targetType + " er ikke støttet");
+        }
+        for (var source : propertySources) {
+            if (source.harVerdi(key)) {
+                Object verdi = source.getVerdi(key, converter);
+                if (verdi != null) {
+                    return (T) verdi;
+                }
+            }
+        }
+        return defaultVerdi;
+    }
+
+    private <T> Converter<?> converterFor(Class<T> targetType) {
+        try {
+            if (targetType.equals(Period.class)) {
+                return KonfigVerdi.PeriodConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(Duration.class)) {
+                return KonfigVerdi.DurationConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(LocalDate.class)) {
+                return KonfigVerdi.LocalDateConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(Long.class)) {
+                return KonfigVerdi.LongConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(Boolean.class) || targetType.equals(boolean.class)) {
+                return KonfigVerdi.BooleanConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(URI.class)) {
+                return KonfigVerdi.UriConverter.class.getDeclaredConstructor().newInstance();
+            }
+            if (targetType.equals(Integer.class) || targetType.equals(int.class)) {
+                return KonfigVerdi.IntegerConverter.class.getDeclaredConstructor().newInstance();
+            }
+            return null;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Uventet feil ved konstruksjon av konverter");
+        }
     }
 
     @Override
--- felles/util/src/test/java/no/nav/vedtak/util/env/EnvironmentTest.java
+++ felles/util/src/test/java/no/nav/vedtak/util/env/EnvironmentTest.java
@@ -3,14 +3,18 @@ package no.nav.vedtak.util.env;
 import static no.nav.vedtak.util.env.Cluster.PROD_FSS;
 import static no.nav.vedtak.util.env.ConfidentialMarkerFilter.CONFIDENTIAL;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.time.Duration;
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import no.nav.vedtak.konfig.Tid;
 
 // Denne testen må kjøres fra maven, ettersom vi ikke enkelt kan sette env properties i kode. 
 public class EnvironmentTest {
@@ -23,9 +27,24 @@ public class EnvironmentTest {
         var stdout = new ByteArrayOutputStream();
         System.setOut(new PrintStream(stdout));
         LOG.info(CONFIDENTIAL, "Dette er konfidensielt, OK i dev men ikke i prod, denne testen kjører i pseudo-prod");
-        assertEquals(ENV.clusterName(), PROD_FSS.clusterName());
+        assertEquals(ENV.getCluster(), PROD_FSS);
         assertEquals("jalla", ENV.namespace());
         assertTrue(ENV.isProd());
         assertEquals(0, stdout.size());
+    }
+
+    @Test
+    public void testPropertiesFraEnv() {
+        assertEquals(Integer.valueOf(50), ENV.getProperty("test2.property", int.class));
+        assertTrue(ENV.getProperty("test4.boolean", boolean.class));
+        assertTrue(ENV.getProperty("test4.boolean", Boolean.class));
+        assertEquals("42", ENV.getProperty("finnes.ikke", "42"));
+        assertEquals(Duration.ofDays(42), ENV.getProperty("duration.property", Duration.class));
+        assertNull(ENV.getProperty("finnes.ikke"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPropertiesFraEnvUkjentConverter() {
+        ENV.getProperty("finnes.ikke", Tid.class);
     }
 }
