40042d004138c080257e13aca08c2fa349321f27
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2019-12-17 18:32:39 +0100

    Legg til klient for saker fra rest mot infotrygd replika

---
+++ integrasjon/infotrygd-saker-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/saker/v1/InfotrygdSakerConsumer.java
@@ -0,0 +1,45 @@
+package no.nav.vedtak.felles.integrasjon.infotrygd.saker.v1;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import org.apache.http.client.utils.URIBuilder;
+
+import no.nav.vedtak.felles.integrasjon.infotrygd.saker.v1.respons.Saker;
+import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
+
+public abstract class InfotrygdSakerConsumer {
+
+    private OidcRestClient restClient;
+    private URI uri;
+
+    public InfotrygdSakerConsumer(OidcRestClient restClient, URI uri) {
+        this.restClient = restClient;
+        this.uri = uri;
+    }
+
+    InfotrygdSakerConsumer() {
+        // Tja, nødvendig her?
+    }
+
+    public Saker getSaker(String fnr, LocalDate fom) throws Exception {
+        Objects.requireNonNull(fnr);
+        var request = new URIBuilder(uri)
+            .addParameter("fnr", fnr)
+            .addParameter("fom", fom(fom))
+            .build();
+        return restClient.get(request, Saker.class);
+    }
+
+    private static String fom(LocalDate fom) {
+        return DateTimeFormatter.ISO_LOCAL_DATE.format(fom);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[restClient=" + restClient + ", uri=" + uri + "]";
+    }
+
+}
