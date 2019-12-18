fa5b90ac526b9c560375733d8a3f88ea9c097548
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2019-12-18 11:00:56 +0100

    Legg til klient for saker fra rest mot infotrygd replika (#180)

---
+++ integrasjon/infotrygd-saker-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/saker/v1/respons/SakResultat.java
@@ -0,0 +1,32 @@
+package no.nav.vedtak.felles.integrasjon.infotrygd.saker.v1.respons;
+
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
+public enum SakResultat {
+    @JsonEnumDefaultValue
+    UKJENT,
+    A,
+    AK,
+    AV,
+    DI,
+    DT,
+    FB,
+    FI,
+    H,
+    HB,
+    I,
+    IN,
+    IS,
+    IT,
+    MO,
+    MT,
+    O,
+    PA,
+    R,
+    SB,
+    TB,
+    TH,
+    TO,
+    UB,
+    Ø;
+}
