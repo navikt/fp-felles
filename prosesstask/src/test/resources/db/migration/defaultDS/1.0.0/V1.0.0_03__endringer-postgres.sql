05c2fa31328683c4c896eb30e9261d644c9f3330
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-01-14 11:06:50 +0100

    fix: fjerner prosesstask fra fp-felles. Bruk fp-prosesstask repo i stedet

--- prosesstask/src/test/resources/db/migration/defaultDS/1.0.0/V1.0.0_03__endringer-postgres.sql
+++
@@ -1,4 +0,0 @@
--- Endringer etter at vi kopierte dette fra FP-ABAKUS
--- https://github.com/navikt/fp-felles/commit/aa13aa084e50fbfcdc99910bef0db66cf89203b5#diff-d86f672fadaf64d7ab6f428177a0fee7
-alter table prosess_task
-    add SISTE_KJOERING_PLUKK_TS TIMESTAMP(6);
