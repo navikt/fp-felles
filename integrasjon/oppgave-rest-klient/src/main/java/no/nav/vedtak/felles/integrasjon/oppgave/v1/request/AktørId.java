81c76d7eaabd97bad0bcb4a4545d800ebf4e010c
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-01-15 08:06:06 +0100

    Update AktørId.java

--- integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/request/AktørId.java
+++ integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/request/AktørId.java
@@ -17,7 +17,7 @@ public class AktørId implements Serializable, Comparable<AktørId> {
     private static final String VALID_REGEXP = "^\\d{13}$";
 
     private static final Pattern VALID = Pattern.compile(VALID_REGEXP, Pattern.CASE_INSENSITIVE);
-    private static AtomicLong DUMMY_AKTØRID = new AtomicLong(1000000000000L);
+    private static final AtomicLong DUMMY_AKTØRID = new AtomicLong(1000000000000L);
 
     @JsonValue
     @NotNull
