6b2064cd7565a53bdaa3f7aed1198ef6a02ab102
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-20 14:29:10 +0100

    log avvik om nødvendig

--- felles/util/src/main/java/no/nav/vedtak/konfig/PropertyUtil.java
+++ felles/util/src/main/java/no/nav/vedtak/konfig/PropertyUtil.java
@@ -1,21 +1,35 @@
 package no.nav.vedtak.konfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.vedtak.util.env.Environment;
+
 public class PropertyUtil {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PropertyUtil.class);
+
+    private static final Environment ENV = Environment.current();
 
     private PropertyUtil() {
     }
 
     /**
      *
-     * @deprecated Bruk heller {@link Environment#getProperty(String)}
+     * @deprecated Bruk heller {@link Environment#getProperty(String)} direkte
      * 
      */
     @Deprecated
     public static String getProperty(String key) {
+        String fraEnv = ENV.getProperty(key);
         String val = System.getProperty(key);
         if (val == null) {
             val = System.getenv(key.toUpperCase().replace('.', '_'));
         }
-        return val;
+        if (fraEnv != val) {
+            LOG.warn("Fikk {} fra ENV, {} på gamlemåten, må undersøkes", fraEnv, val);
+            return val; // gammel logikk vinner mens vi undersøker hvorfor.
+        }
+        return fraEnv;
     }
 }
