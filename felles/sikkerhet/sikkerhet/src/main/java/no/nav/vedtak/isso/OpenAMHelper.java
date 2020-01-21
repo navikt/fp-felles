7ff9fcf87900d98449881fb190f884839c78c802
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-21 14:55:23 +0100

    log

--- felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/isso/OpenAMHelper.java
+++ felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/isso/OpenAMHelper.java
@@ -1,27 +1,5 @@
 package no.nav.vedtak.isso;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.MapType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import no.nav.vedtak.isso.config.ServerInfo;
-import no.nav.vedtak.sikkerhet.domene.IdTokenAndRefreshToken;
-import no.nav.vedtak.sikkerhet.oidc.IdTokenAndRefreshTokenProvider;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.glassfish.json.JsonUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-import javax.json.JsonObject;
-
 import static no.nav.vedtak.konfig.PropertyUtil.getProperty;
 
 import java.io.BufferedReader;
@@ -37,6 +15,29 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import javax.json.JsonObject;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.glassfish.json.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import no.nav.vedtak.isso.config.ServerInfo;
+import no.nav.vedtak.sikkerhet.domene.IdTokenAndRefreshToken;
+import no.nav.vedtak.sikkerhet.oidc.IdTokenAndRefreshTokenProvider;
 
 public class OpenAMHelper {
 
@@ -60,9 +61,11 @@ public class OpenAMHelper {
 
     public OpenAMHelper() {
         try {
-            redirectUriEncoded = URLEncoder.encode(ServerInfo.instance().getCallbackUrl(), StandardCharsets.UTF_8.name());
+            redirectUriEncoded = URLEncoder.encode(ServerInfo.instance().getCallbackUrl(),
+                    StandardCharsets.UTF_8.name());
         } catch (UnsupportedEncodingException e) {
-            throw OpenAmFeil.FACTORY.feilIKonfigurertRedirectUri(ServerInfo.instance().getCallbackUrl(), e).toException();
+            throw OpenAmFeil.FACTORY.feilIKonfigurertRedirectUri(ServerInfo.instance().getCallbackUrl(), e)
+                    .toException();
         }
     }
 
@@ -86,20 +89,22 @@ public class OpenAMHelper {
             throw new IllegalArgumentException("Brukernavn og/eller passord mangler.");
         }
         CookieStore cookieStore = new BasicCookieStore();
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling().setDefaultCookieStore(cookieStore).build()) {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling()
+                .setDefaultCookieStore(cookieStore).build()) {
             authenticateUser(httpClient, cookieStore, brukernavn, passord);
             String authorizationCode = hentAuthorizationCode(httpClient);
 
-            return new IdTokenAndRefreshTokenProvider().getToken(authorizationCode, URI.create(ServerInfo.instance().getCallbackUrl()));
+            return new IdTokenAndRefreshTokenProvider().getToken(authorizationCode,
+                    URI.create(ServerInfo.instance().getCallbackUrl()));
         }
     }
 
-    private void authenticateUser(CloseableHttpClient httpClient, CookieStore cookieStore, String brukernavn, String passord) throws IOException {
+    private void authenticateUser(CloseableHttpClient httpClient, CookieStore cookieStore, String brukernavn,
+            String passord) throws IOException {
         String jsonAuthUrl = getIssoHostUrl().replace(OAUTH2_ENDPOINT, JSON_AUTH_ENDPOINT);
 
         String template = post(httpClient, jsonAuthUrl, null, Function.identity(),
-            "Authorization: Negotiate");
-
+                "Authorization: Negotiate");
         ObjectMapper mapper = new ObjectMapper();
         String utfyltTemplate;
         try {
@@ -125,11 +130,13 @@ public class OpenAMHelper {
         cookieStore.addCookie(cookie);
     }
 
-    private <T> T post(CloseableHttpClient httpClient, String url, String data, Function<String, T> resultTransformer, String... headers) throws IOException {
+    private <T> T post(CloseableHttpClient httpClient, String url, String data, Function<String, T> resultTransformer,
+            String... headers) throws IOException {
         return post(httpClient, url, data, 200, resultTransformer, headers);
     }
 
-    private <T> T post(CloseableHttpClient httpClient, String url, String data, int expectedHttpCode, Function<String, T> resultTransformer, String... headers) throws IOException {
+    private <T> T post(CloseableHttpClient httpClient, String url, String data, int expectedHttpCode,
+            Function<String, T> resultTransformer, String... headers) throws IOException {
         HttpPost post = new HttpPost(url);
         post.setHeader("Content-type", "application/json");
         for (String header : headers) {
@@ -140,14 +147,20 @@ public class OpenAMHelper {
             post.setEntity(new StringEntity(data, "UTF-8"));
         }
 
+        LOGGER.info("POST mot URL {}", url);
+
         try (CloseableHttpResponse response = httpClient.execute(post)) {
-            try (InputStreamReader isr = new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8)) {
+            try (InputStreamReader isr = new InputStreamReader(response.getEntity().getContent(),
+                    StandardCharsets.UTF_8)) {
                 try (BufferedReader br = new BufferedReader(isr)) {
                     String responseString = br.lines().collect(Collectors.joining("\n"));
                     if (response.getStatusLine().getStatusCode() == expectedHttpCode) {
+                        LOGGER.info("POST mot URL OK {}", url);
                         return resultTransformer.apply(responseString);
                     } else {
-                        throw OpenAmFeil.FACTORY.uforventetResponsFraOpenAM(response.getStatusLine().getStatusCode(), responseString).toException();
+                        throw OpenAmFeil.FACTORY
+                                .uforventetResponsFraOpenAM(response.getStatusLine().getStatusCode(), responseString)
+                                .toException();
                     }
                 }
             }
@@ -169,15 +182,18 @@ public class OpenAMHelper {
         String url = getIssoHostUrl() + WELL_KNOWN_ENDPOINT;
         if (wellKnownConfig == null) {
             HttpGet get = new HttpGet(url);
+            LOGGER.info("GET mot URL {}", url);
             try (CloseableHttpResponse response = httpClient.execute(get)) {
-                try (InputStreamReader isr = new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8)) {
+                try (InputStreamReader isr = new InputStreamReader(response.getEntity().getContent(),
+                        StandardCharsets.UTF_8)) {
                     try (BufferedReader br = new BufferedReader(isr)) {
                         String responseString = br.lines().collect(Collectors.joining("\n"));
                         if (response.getStatusLine().getStatusCode() == 200) {
-                            LOGGER.debug("Successfully fetching well known oidc configuration from " + url);
+                            LOGGER.info("Successfully fetching well known oidc configuration from " + url);
                             setWellKnownConfig(responseString);
                         } else {
-                            throw OpenAmFeil.FACTORY.uforventetResponsFraOpenAM(response.getStatusLine().getStatusCode(), responseString).toException();
+                            throw OpenAmFeil.FACTORY.uforventetResponsFraOpenAM(
+                                    response.getStatusLine().getStatusCode(), responseString).toException();
                         }
                     }
                 }
@@ -199,20 +215,25 @@ public class OpenAMHelper {
     }
 
     private String hentAuthorizationCode(CloseableHttpClient httpClient) throws IOException {
-        String url = getAuthorizationEndpoint() + "?response_type=code&scope=openid&client_id=" + getIssoUserName() + "&state=dummy&redirect_uri=" + redirectUriEncoded;
+        String url = getAuthorizationEndpoint() + "?response_type=code&scope=openid&client_id=" + getIssoUserName()
+                + "&state=dummy&redirect_uri=" + redirectUriEncoded;
         HttpGet get = new HttpGet(url);
         get.setHeader("Content-type", "application/json");
 
+        LOGGER.info("GET mot URL {}", url);
         try (CloseableHttpResponse response = httpClient.execute(get)) {
             if (response.containsHeader("Location")) {
                 Pattern pattern = Pattern.compile("code=([^&]*)");
                 String locationHeader = response.getFirstHeader("Location").getValue();
                 Matcher matcher = pattern.matcher(locationHeader);
                 if (matcher.find()) {
+                    LOGGER.info("GET mot URL OK {}", url);
                     return matcher.group(1);
                 }
             }
-            throw OpenAmFeil.FACTORY.kunneIkkeFinneAuthCode(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase()).toException();
+            LOGGER.info("GET mot URL feilet {}", url);
+            throw OpenAmFeil.FACTORY.kunneIkkeFinneAuthCode(response.getStatusLine().getStatusCode(),
+                    response.getStatusLine().getReasonPhrase()).toException();
         } finally {
             get.reset();
         }
@@ -240,6 +261,11 @@ public class OpenAMHelper {
 
     public static String getAuthorizationEndpoint() {
         return getStringFromWellKnownConfig(AUTHORIZATION_ENDPOINT_KEY);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[redirectUriEncoded=" + redirectUriEncoded + "]";
     }
 
 }
