f4cb2ca0e2be5f005d4f7a182f59301c01fde236
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-02-11 16:38:22 +0100

    TFP-1201 fjerner integrasjoner mot infotrygd ws

--- integrasjon/infotrygdberegningsgrunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygdberegningsgrunnlag/InfotrygdBeregningsgrunnlagSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.infotrygdberegningsgrunnlag;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface InfotrygdBeregningsgrunnlagSelftestConsumer extends SelftestConsumer {
-}
