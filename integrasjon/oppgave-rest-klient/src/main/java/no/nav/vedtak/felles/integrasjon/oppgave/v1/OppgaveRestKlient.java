8ca0540c318a621bcc772e13ef50605f1a4e4afd
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-04-23 19:21:18 +0200

    TFP-2936 felles oppgaveklient. Validert i P for fpfordel og fpsak

---
+++ integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/OppgaveRestKlient.java
@@ -0,0 +1,86 @@
+package no.nav.vedtak.felles.integrasjon.oppgave.v1;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.http.Header;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicHeader;
+
+import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
+import no.nav.vedtak.konfig.KonfigVerdi;
+import no.nav.vedtak.log.mdc.MDCOperations;
+
+@ApplicationScoped
+public class OppgaveRestKlient {
+
+    private static final String ENDPOINT_KEY = "oppgave.rs.uri";
+    private static final String DEFAULT_URI = "http://oppgave.default/api/v1/oppgaver";
+    private static final String HEADER_CORRELATION_ID = "X-Correlation-ID";
+    private static final String STATUSKATEGORI_AAPEN = "AAPEN";
+
+    private OidcRestClient oidcRestClient;
+    private URI endpoint;
+
+    public OppgaveRestKlient() {
+    }
+
+    @Inject
+    public OppgaveRestKlient(OidcRestClient oidcRestClient,
+                             @KonfigVerdi(value = ENDPOINT_KEY, defaultVerdi = DEFAULT_URI) URI endpoint) {
+        this.oidcRestClient = oidcRestClient ;
+        this.endpoint = endpoint;
+    }
+
+    public Oppgave opprettetOppgave(OpprettOppgave.Builder requestBuilder) {
+        return oidcRestClient.post(endpoint, requestBuilder.build(), lagHeader(), Oppgave.class);
+    }
+
+    public List<Oppgave> finnAlleOppgaver(String aktørId, String tema, List<String> oppgaveTyper) throws Exception {
+        var builder = new URIBuilder(endpoint).addParameter("aktoerId", aktørId);
+        if (tema != null)
+            builder.addParameter("tema", tema);
+        oppgaveTyper.forEach(ot -> builder.addParameter("oppgavetype", ot));
+        return oidcRestClient.get(builder.build(), lagHeader(), FinnOppgaveResponse.class).getOppgaver();
+    }
+
+    public List<Oppgave> finnÅpneOppgaver(String aktørId, String tema, List<String> oppgaveTyper) throws Exception {
+        var builder = new URIBuilder(endpoint)
+            .addParameter("aktoerId", aktørId)
+            .addParameter("statuskategori", STATUSKATEGORI_AAPEN);
+        if (tema != null)
+            builder.addParameter("tema", tema);
+        oppgaveTyper.forEach(ot -> builder.addParameter("oppgavetype", ot));
+        return oidcRestClient.get(builder.build(), lagHeader(), FinnOppgaveResponse.class).getOppgaver();
+    }
+
+    public void ferdigstillOppgave(String oppgaveId) {
+        var oppgave = hentOppgave(oppgaveId);
+        var patch = new PatchOppgave(oppgave.getId(), oppgave.getVersjon(), Oppgavestatus.FERDIGSTILT);
+        oidcRestClient.patch(getEndpointForOppgaveId(oppgaveId), patch, lagHeader());
+    }
+
+    public void feilregistrerOppgave(String oppgaveId) {
+        var oppgave = hentOppgave(oppgaveId);
+        var patch = new PatchOppgave(oppgave.getId(), oppgave.getVersjon(), Oppgavestatus.FEILREGISTRERT);
+        oidcRestClient.patch(getEndpointForOppgaveId(oppgaveId), patch, lagHeader());
+    }
+
+    public Oppgave hentOppgave(String oppgaveId) {
+        return oidcRestClient.get(getEndpointForOppgaveId(oppgaveId), lagHeader(), Oppgave.class);
+    }
+
+    private URI getEndpointForOppgaveId(String oppgaveId) {
+        return URI.create(endpoint.toString() + "/" + oppgaveId);
+    }
+
+    private Set<Header> lagHeader() {
+        return Collections.singleton(new BasicHeader(HEADER_CORRELATION_ID, MDCOperations.getCallId()));
+    }
+
+}
---
+++ integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/Prioritet.java
@@ -0,0 +1,10 @@
+package no.nav.vedtak.felles.integrasjon.oppgave.v1;
+
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
+public enum Prioritet {
+    HOY,
+    @JsonEnumDefaultValue
+    NORM,
+    LAV
+}
