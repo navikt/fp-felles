341780c451eed48e251d6dd11cc4b05569d54f4a
Michal J. Sladek <michal.sladek@nav.no>
2020-04-24 08:47:07 +0200

    FIX: Oppretter ny socket da dette er ikke mulig aa gjenaapne, logg level warn.

--- integrasjon/sensu-klient/src/main/java/no/nav/vedtak/felles/integrasjon/sensu/SensuKlient.java
+++ integrasjon/sensu-klient/src/main/java/no/nav/vedtak/felles/integrasjon/sensu/SensuKlient.java
@@ -22,7 +22,7 @@ public class SensuKlient {
 
     private static final Logger LOG = LoggerFactory.getLogger(SensuKlient.class);
     private static final ExecutorService executorService = Executors.newFixedThreadPool(2);
-    private static final Socket socket = new Socket();
+    private Socket socket;
 
     private String sensuHost;
     private int sensuPort;
@@ -36,6 +36,7 @@ public class SensuKlient {
                        @KonfigVerdi(value = "SENSU_PORT", defaultVerdi = "3030") Integer sensuPort) {
         this.sensuHost = sensuHost;
         this.sensuPort = sensuPort;
+        this.socket = new Socket();
     }
 
     public void logMetrics(SensuEvent metrics) {
@@ -57,11 +58,11 @@ public class SensuKlient {
                     writer.flush();
                     LOG.debug("Skrev {} bytes med data.", data.length());
                 } catch (IOException e) {
-                    LOG.error("Feil ver sending av event {}", sensuEvent, e);
+                    LOG.warn("Feil ver sending av event {}", sensuEvent, e);
                 }
             } catch (Exception ex) {
                 if (System.getenv("NAIS_CLUSTER_NAME") != null) {
-                    LOG.error("Feil ved tilkobling til metrikkendepunktet", ex);
+                    LOG.warn("Feil ved tilkobling til metrikkendepunktet", ex);
                 }
             } finally {
                 long tidBrukt = System.currentTimeMillis() - startTs;
@@ -73,12 +74,13 @@ public class SensuKlient {
     private Socket establishSocketConnectionIfNeeded() throws Exception {
         if (socket.isClosed() || !socket.isConnected()) {
             try {
+                socket = new Socket();
                 socket.setSoTimeout(1000);
                 socket.connect(new InetSocketAddress(sensuHost, sensuPort), 1000);
                 return socket;
             } catch (Exception ex) {
                 final String feilMelding = "Feil ved start av socket tilkobling.";
-                LOG.error(feilMelding, ex);
+                LOG.debug(feilMelding, ex);
                 throw new Exception(feilMelding, ex);
             }
         }
