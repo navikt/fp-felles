06ea2eb7de6f208a04bc97cc1b4211d7ca64ce90
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-12 12:09:13 +0200

    fix: fjernet behandlejournal-klient, oppgave-klient, behandleoppgave-klient, journal-klient v2(soap) og arbeidsfordeling (soap)

--- integrasjon/behandleoppgave-klient/src/main/java/no/nav/vedtak/felles/integrasjon/behandleoppgave/BrukerType.java
+++
@@ -1,5 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.behandleoppgave;
-
-public enum BrukerType {
-    PERSON, ORGANISASJON, SAMHANDLER, APPBRUKER, BLANK
-}
--- integrasjon/journal-klient/src/main/java/no/nav/vedtak/felles/integrasjon/journal/v2/JournalSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.journal.v2;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface JournalSelftestConsumer extends SelftestConsumer {
-}
