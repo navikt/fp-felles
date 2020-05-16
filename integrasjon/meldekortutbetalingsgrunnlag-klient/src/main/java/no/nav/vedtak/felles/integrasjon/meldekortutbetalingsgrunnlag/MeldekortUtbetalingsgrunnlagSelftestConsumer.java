1d6884b7a60d60886128a2901ace3b5de00fed6e
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-16 10:52:56 +0200

    fix: fjerner meldekortutbetalingsgrunnlag * ekskluderer woodstox-core-asl siden den kolliderer med woodstox-core og er ikke oppdatert siden 2014

--- integrasjon/meldekortutbetalingsgrunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/meldekortutbetalingsgrunnlag/MeldekortUtbetalingsgrunnlagSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.meldekortutbetalingsgrunnlag;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface MeldekortUtbetalingsgrunnlagSelftestConsumer extends SelftestConsumer {
-}
