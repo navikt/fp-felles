f83bbf2934dd4cc4f46dbde142041a50eb0e0c5d
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-05-26 13:18:31 +0200

    fix: oppgraderer til v1.9 fjerner behandleinngaaendejournal-klient fjerner inngaaendejournal-klient fjerner mottainngaendeforsendelse-klient

--- integrasjon/behandleinngaaendejournal-klient/src/main/java/no/nav/vedtak/felles/integrasjon/behandleinngaaendejournal/BehandleInngaaendeJournalSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.behandleinngaaendejournal;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface BehandleInngaaendeJournalSelftestConsumer extends SelftestConsumer {
-}
--- integrasjon/mottainngaaendeforsendelse-klient/src/main/java/no/nav/vedtak/felles/integrasjon/mottainngaaendeforsendelse/MottaInngaaendeForsendelseRestKlient.java
+++
@@ -1,8 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.mottainngaaendeforsendelse;
-
-import no.nav.dok.tjenester.mottainngaaendeforsendelse.MottaInngaaendeForsendelseRequest;
-import no.nav.dok.tjenester.mottainngaaendeforsendelse.MottaInngaaendeForsendelseResponse;
-
-public interface MottaInngaaendeForsendelseRestKlient {
-    MottaInngaaendeForsendelseResponse journalførForsendelse(MottaInngaaendeForsendelseRequest request);
-}
