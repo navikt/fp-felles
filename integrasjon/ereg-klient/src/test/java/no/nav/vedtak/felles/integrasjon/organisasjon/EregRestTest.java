56015afb44249500894dbf1aa32f871bc1deeca9
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-06-09 23:32:39 +0200

    NOJIRA ignore unknown

--- integrasjon/ereg-klient/src/test/java/no/nav/vedtak/felles/integrasjon/organisasjon/EregRestTest.java
+++ integrasjon/ereg-klient/src/test/java/no/nav/vedtak/felles/integrasjon/organisasjon/EregRestTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -107,6 +108,7 @@ public class EregRestTest {
     public static void setup() {
         mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     @Test
