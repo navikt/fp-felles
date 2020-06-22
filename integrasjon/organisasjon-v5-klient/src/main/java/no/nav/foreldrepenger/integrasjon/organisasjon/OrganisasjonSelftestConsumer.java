2be999a7c139ecbfe5e72152100db3fad1927882
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-06-22 12:07:23 +0200

    fix: fjerner organisasajon v4/v5 fra felles (bruk ereg klient i stedet)

--- integrasjon/organisasjon-v5-klient/src/main/java/no/nav/foreldrepenger/integrasjon/organisasjon/OrganisasjonSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.foreldrepenger.integrasjon.organisasjon;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface OrganisasjonSelftestConsumer extends SelftestConsumer {
-}
