4970a778421d88b22fb0456b6ec48470b256817a
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-08-31 08:58:42 +0200

    NOJIRA fjerne arbeidsforhold WS (#449)
    Co-authored-by: Frode Carlsen <frode.odde.carlsen@nav.no>

--- integrasjon/arbeidsforhold-klient/src/main/java/no/nav/vedtak/felles/integrasjon/arbeidsforhold/ArbeidsforholdSelftestConsumer.java
+++
@@ -1,6 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.arbeidsforhold;
-
-import no.nav.vedtak.felles.integrasjon.felles.ws.SelftestConsumer;
-
-public interface ArbeidsforholdSelftestConsumer extends SelftestConsumer {
-}
