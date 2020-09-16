3059e7c8b538ee18e7954eb50ee2728d1d4641fa
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-09-16 12:19:52 +0200

    logsniffer replacement

--- felles/sikkerhet/sikkerhet/src/test/java/no/nav/vedtak/isso/config/ServerInfoTest.java
+++ felles/sikkerhet/sikkerhet/src/test/java/no/nav/vedtak/isso/config/ServerInfoTest.java
@@ -1,5 +1,6 @@
 package no.nav.vedtak.isso.config;
 
+import static no.nav.vedtak.isso.config.ServerInfo.PROPERTY_KEY_LOADBALANCER_URL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.AfterEach;
@@ -42,28 +43,28 @@ public class ServerInfoTest {
 
     @Test
     public void skalGenerereGyldigCallbackURL() throws Exception {
-        System.setProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL, "http://localhost:8080");
+        System.setProperty(PROPERTY_KEY_LOADBALANCER_URL, "http://localhost:8080");
         ServerInfo serverinfo = new ServerInfo();
         assertThat(serverinfo.getCallbackUrl()).isEqualTo("http://localhost:8080/fpsak/cb");
-        System.clearProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL);
+        System.clearProperty(PROPERTY_KEY_LOADBALANCER_URL);
     }
 
     @Test
     public void skal_hente_cookie_domain_fra_loadbalancerUrl_og_utvide_ett_nivå() throws Exception {
-        System.setProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL, "https://bar.nav.no");
+        System.setProperty(PROPERTY_KEY_LOADBALANCER_URL, "https://bar.nav.no");
         assertThat(new ServerInfo().getCookieDomain()).isEqualTo("nav.no");
-        System.setProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL, "https://baz.devillo.no");
+        System.setProperty(PROPERTY_KEY_LOADBALANCER_URL, "https://baz.devillo.no");
         assertThat(new ServerInfo().getCookieDomain()).isEqualTo("devillo.no");
-        System.clearProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL);
+        System.clearProperty(PROPERTY_KEY_LOADBALANCER_URL);
     }
 
     @Test
     public void skal_sett_cookie_domain_til_null_når_domenet_er_for_smalt_til_å_utvides_og_logge_dette() throws Exception {
-        System.setProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL, "https://nav.no");
+        System.setProperty(PROPERTY_KEY_LOADBALANCER_URL, "https://nav.no");
         assertThat(new ServerInfo().getCookieDomain()).isNull();
-        System.setProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL, "http://localhost:8080");
+        System.setProperty(PROPERTY_KEY_LOADBALANCER_URL, "http://localhost:8080");
         assertThat(new ServerInfo().getCookieDomain()).isNull();
-        System.clearProperty(ServerInfo.PROPERTY_KEY_LOADBALANCER_URL);
+        System.clearProperty(PROPERTY_KEY_LOADBALANCER_URL);
         assertThat(logSniffer.search("Uventet format for host", Level.WARN)).hasSize(2);
     }
 }
