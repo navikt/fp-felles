5bfc2f4d4c3cd4e36fb98c9a9627d964536287ce
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-20 15:17:42 +0100

    filter for heade logging in non-prod

--- integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/jersey/HeaderLoggingFilter.java
+++ integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/jersey/HeaderLoggingFilter.java
@@ -8,14 +8,17 @@ import javax.ws.rs.client.ClientRequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.vedtak.util.env.Environment;
+
 class HeaderLoggingFilter implements ClientRequestFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(HeaderLoggingFilter.class);
 
     @Override
     public void filter(ClientRequestContext ctx) throws IOException {
-        ctx.getHeaders().entrySet().stream()
-                .forEach(e -> LOG.trace("{} -> {}", e.getKey(), e.getValue()));
+        if (!Environment.current().isProd()) {
+            ctx.getHeaders().entrySet().stream()
+                    .forEach(e -> LOG.trace("{} -> {}", e.getKey(), e.getValue()));
+        }
     }
-
 }
