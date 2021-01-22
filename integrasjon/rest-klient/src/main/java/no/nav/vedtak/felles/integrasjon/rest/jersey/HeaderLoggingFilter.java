6ad6e7311df86e43f2bb664e2cca2e0d73e88789
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-22 20:42:14 +0100

    log

--- integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/jersey/HeaderLoggingFilter.java
+++ integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/jersey/HeaderLoggingFilter.java
@@ -4,18 +4,28 @@ import java.io.IOException;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.vedtak.util.env.Environment;
 
-class HeaderLoggingFilter implements ClientRequestFilter {
+class HeaderLoggingFilter implements ClientRequestFilter, ClientResponseFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(HeaderLoggingFilter.class);
 
     @Override
     public void filter(ClientRequestContext ctx) throws IOException {
+        if (!Environment.current().isProd()) {
+            ctx.getHeaders().entrySet().stream()
+                    .forEach(e -> LOG.trace("{} -> {}", e.getKey(), e.getValue()));
+        }
+    }
+
+    @Override
+    public void filter(ClientRequestContext ctx, ClientResponseContext responseContext) throws IOException {
         if (!Environment.current().isProd()) {
             ctx.getHeaders().entrySet().stream()
                     .forEach(e -> LOG.trace("{} -> {}", e.getKey(), e.getValue()));
