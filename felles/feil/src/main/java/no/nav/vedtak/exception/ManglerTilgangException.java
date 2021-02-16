ab3d442fef04da3e01e0cfa5119ab32139b5f2f6
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-02-16 20:14:38 +0100

    fix ManglerTilgangException

--- felles/feil/src/main/java/no/nav/vedtak/exception/ManglerTilgangException.java
+++ felles/feil/src/main/java/no/nav/vedtak/exception/ManglerTilgangException.java
@@ -1,31 +1,27 @@
 package no.nav.vedtak.exception;
 
-import static java.lang.String.format;
-
-import java.net.URI;
+import static no.nav.vedtak.feil.LogLevel.WARN;
 
 import no.nav.vedtak.feil.Feil;
 import no.nav.vedtak.feil.LogLevel;
 
 public class ManglerTilgangException extends VLException {
 
-    private static final String KODE = "F-468815";
-    private static final String DEFAULT_MSG = "Mangler tilgang. Fikk http-kode 403 fra server %s";
+    public ManglerTilgangException(String kode, String msg) {
+        this(kode, msg, null);
+    }
 
-    /**
-     *
-     * @deprecated Lag med new og args.
-     */
+    public ManglerTilgangException(String kode, String msg, Throwable cause) {
+        this(kode, msg, WARN, ManglerTilgangException.class, cause);
+    }
+
+    private ManglerTilgangException(String kode, String msg, LogLevel level, Class<? extends VLException> clazz, Throwable cause) {
+        this(new Feil(kode, msg, level, clazz, cause));
+    }
+
     @Deprecated
     public ManglerTilgangException(Feil feil) {
         super(feil);
     }
 
-    public ManglerTilgangException(URI endpoint, LogLevel level, Throwable cause) {
-        this(new Feil(KODE, format(DEFAULT_MSG, endpoint), level, ManglerTilgangException.class, cause));
-    }
-
-    public ManglerTilgangException(URI endpoint) {
-        this(endpoint, LogLevel.ERROR, null);
-    }
 }
