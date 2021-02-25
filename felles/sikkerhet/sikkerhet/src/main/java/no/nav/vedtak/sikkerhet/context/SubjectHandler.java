cfdf2fc72783fe119df3e7274310238508813420
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-02-25 11:55:43 +0100

    Velkommen til 2021

--- felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/context/SubjectHandler.java
+++ felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/context/SubjectHandler.java
@@ -1,13 +1,11 @@
 package no.nav.vedtak.sikkerhet.context;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.security.auth.Subject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.sikkerhet.domene.AuthenticationLevelCredential;
@@ -19,40 +17,30 @@ import no.nav.vedtak.sikkerhet.domene.SluttBruker;
 import no.nav.vedtak.util.env.Environment;
 
 public abstract class SubjectHandler {
+    public abstract Subject getSubject();
 
     private static final Environment ENV = Environment.current();
 
-    private static final Logger logger = LoggerFactory.getLogger(SubjectHandler.class);
-
-    public static final String SUBJECTHANDLER_KEY = "no.nav.modig.core.context.subjectHandlerImplementationClass";
+    static final String SUBJECTHANDLER_KEY = "no.nav.modig.core.context.subjectHandlerImplementationClass";
 
     public static SubjectHandler getSubjectHandler() {
 
-        String subjectHandlerImplementationClass = ENV.getProperty(SUBJECTHANDLER_KEY, JettySubjectHandler.class.getName());
-
         try {
-            Class<?> clazz = Class.forName(subjectHandlerImplementationClass);
-            // For mye støy. Slå på ved behov. logger.debug("Creating a SubjectHandler of
-            // type: {}", subjectHandlerImplementationClass);
-            return (SubjectHandler) clazz.getDeclaredConstructor().newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            return SubjectHandler.class.cast(
+                    Class.forName(ENV.getProperty(SUBJECTHANDLER_KEY, JettySubjectHandler.class.getName())).getDeclaredConstructor().newInstance());
+        } catch (Exception e) {
             throw new IllegalStateException("Klarte ikke å konfigurere plattformavhengig SubjectHandler", e);
         }
     }
-
-    public abstract Subject getSubject();
 
     public String getUid() {
         return getUid(getSubject());
     }
 
     public static String getUid(Subject subject) {
-        SluttBruker sluttBruker = getSluttBruker(subject);
-        if (sluttBruker != null) {
-            return sluttBruker.getName();
-        }
-
-        return null;
+        return Optional.ofNullable(getSluttBruker(subject))
+                .map(SluttBruker::getName)
+                .orElse(null);
     }
 
     public SluttBruker getSluttBruker() {
@@ -60,59 +48,43 @@ public abstract class SubjectHandler {
     }
 
     public static SluttBruker getSluttBruker(Subject subject) {
-        if (subject == null) {
-            return null;
-        }
-
-        SluttBruker sluttBruker = getTheOnlyOneInSet(subject.getPrincipals(SluttBruker.class));
-        if (sluttBruker != null) {
-            return sluttBruker;
-        }
-
-        return null;
+        return Optional.ofNullable(subject)
+                .map(s -> s.getPrincipals(SluttBruker.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .orElse(null);
     }
 
     public IdentType getIdentType() {
-        if (!hasSubject()) {
-            return null;
-        }
-
-        SluttBruker sluttBruker = getTheOnlyOneInSet(getSubject().getPrincipals(SluttBruker.class));
-        if (sluttBruker != null) {
-            return sluttBruker.getIdentType();
-        }
-
-        return null;
+        return Optional.ofNullable(getSubject())
+                .map(s -> s.getPrincipals(SluttBruker.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .map(SluttBruker::getIdentType)
+                .orElse(null);
     }
 
     public String getInternSsoToken() {
-        if (!hasSubject()) {
-            return null;
-        }
-        // TODO (u139158): PK-41761 Flyttes til privateCredentials
-        OidcCredential tokenCredential = getTheOnlyOneInSet(getSubject().getPublicCredentials(OidcCredential.class));
-        return tokenCredential != null ? tokenCredential.getToken() : null;
+        return Optional.ofNullable(getSubject())
+                .map(s -> s.getPublicCredentials(OidcCredential.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .filter(Objects::nonNull)
+                .map(OidcCredential::getToken)
+                .orElse(null);
     }
 
     public SAMLAssertionCredential getSamlToken() {
-        if (!hasSubject()) {
-            return null;
-        }
-        return getTheOnlyOneInSet(getSubject().getPublicCredentials(SAMLAssertionCredential.class));
+        return Optional.ofNullable(getSubject())
+                .map(s -> s.getPublicCredentials(SAMLAssertionCredential.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .orElse(null);
     }
 
     public Integer getAuthenticationLevel() {
-        if (!hasSubject()) {
-            return null;
-        }
-
-        AuthenticationLevelCredential authenticationLevelCredential = getTheOnlyOneInSet(
-                getSubject().getPublicCredentials(AuthenticationLevelCredential.class));
-        if (authenticationLevelCredential != null) {
-            return authenticationLevelCredential.getAuthenticationLevel();
-        }
-
-        return null;
+        return Optional.ofNullable(getSubject())
+                .map(s -> s.getPublicCredentials(AuthenticationLevelCredential.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .filter(Objects::nonNull)
+                .map(AuthenticationLevelCredential::getAuthenticationLevel)
+                .orElse(null);
     }
 
     public String getConsumerId() {
@@ -120,16 +92,12 @@ public abstract class SubjectHandler {
     }
 
     public static String getConsumerId(Subject subject) {
-        if (subject == null) {
-            return null;
-        }
-
-        ConsumerId consumerId = getTheOnlyOneInSet(subject.getPrincipals(ConsumerId.class));
-        if (consumerId != null) {
-            return consumerId.getConsumerId();
-        }
-
-        return null;
+        return Optional.ofNullable(subject)
+                .map(s -> s.getPrincipals(ConsumerId.class))
+                .map(SubjectHandler::getTheOnlyOneInSet)
+                .filter(Objects::nonNull)
+                .map(ConsumerId::getConsumerId)
+                .orElse(null);
     }
 
     private static <T> T getTheOnlyOneInSet(Set<T> set) {
@@ -137,9 +105,8 @@ public abstract class SubjectHandler {
             return null;
         }
 
-        T first = set.iterator().next();
         if (set.size() == 1) {
-            return first;
+            return set.iterator().next();
         }
 
         // logging class names to the log to help debug. Cannot log actual objects,
@@ -149,16 +116,7 @@ public abstract class SubjectHandler {
                 .map(Class::getName)
                 .collect(Collectors.toSet());
         throw new TekniskException("F-327190",
-        String.format("Forventet ingen eller ett element, men fikk %s elementer av type %s", set.size(), classNames));
-    }
-
-    private Boolean hasSubject() {
-        return getSubject() != null;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+                String.format("Forventet ingen eller ett element, men fikk %s elementer av type %s", set.size(), classNames));
     }
 
 }
