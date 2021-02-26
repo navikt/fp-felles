b9f8dc4ace21cb3634da67a5de41af3cf6e1219f
Marius Bækken Glittum <marius.glittum@nav.no>
2021-02-26 08:16:28 +0100

    Feature/mulig å sette cookiepath (#690)
    * Muliggjør å sette cookiepath på idToken, default oppførsel er som før men kan settes via ContextPathHolder

--- felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/ContextPathHolder.java
+++ felles/sikkerhet/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/ContextPathHolder.java
@@ -1,12 +1,24 @@
 package no.nav.vedtak.sikkerhet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ContextPathHolder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContextPathHolder.class);
 
     private static volatile ContextPathHolder instance; // NOSONAR
     private final String contextPath;
+    private final String cookiePath;
 
     private ContextPathHolder(String contextPath) {
         this.contextPath = contextPath;
+        this.cookiePath = validerCookiePath(null);
+    }
+
+    public ContextPathHolder(String contextPath, String cookiePath) {
+        this.contextPath = contextPath;
+        this.cookiePath = validerCookiePath(cookiePath);
     }
 
     public static ContextPathHolder instance() {
@@ -26,8 +38,31 @@ public class ContextPathHolder {
         return inst;
     }
 
+    public static ContextPathHolder instance(String contextPath, String cookiePath) {
+        var inst = instance;
+        if (inst == null) {
+            inst = new ContextPathHolder(contextPath, cookiePath);  // NOSONAR trenger ikke synkronisering her
+            instance = inst;
+        }
+        return inst;
+    }
+
+    private String validerCookiePath(String cookiePath) {
+        if (cookiePath == null) {
+            return "/";
+        }
+        if (!cookiePath.startsWith("/")) {
+            LOG.warn("CookiePath ({}) er ugyldig som cookiePath, forkaster og bruker default ('/').", cookiePath);
+            return "/";
+        }
+        return cookiePath;
+    }
+
     public String getContextPath() {
         return contextPath;
     }
 
+    public String getCookiePath() {
+        return cookiePath;
+    }
 }
