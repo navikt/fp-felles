4cd4dae779438e71009467e83d5d684bd4a9f82b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-19 11:40:10 +0200

    cleanup

--- integrasjon/pdl-klient/src/main/java/no/nav/vedtak/felles/integrasjon/pdl/OnBehalfOfJerseyPdlKlient.java
+++ integrasjon/pdl-klient/src/main/java/no/nav/vedtak/felles/integrasjon/pdl/OnBehalfOfJerseyPdlKlient.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import javax.ws.rs.client.ClientRequestFilter;
 
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.felles.integrasjon.rest.jersey.Jersey;
@@ -20,8 +21,8 @@ public class OnBehalfOfJerseyPdlKlient extends AbstractJerseyPdlKlient {
         this(endpoint, new TokenXRequestFilter(tema));
     }
 
-    OnBehalfOfJerseyPdlKlient(URI endpoint, TokenXRequestFilter tokenXTokenRequestFilter) {
-        super(endpoint, tokenXTokenRequestFilter);
+    OnBehalfOfJerseyPdlKlient(URI endpoint, ClientRequestFilter filter) {
+        super(endpoint, filter);
     }
 
 }
