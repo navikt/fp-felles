a04a4adf2608681df76f684adbc0cade49d3cad5
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-06 11:17:42 +0200

    flere metrikker

--- felles/log/src/main/java/no/nav/vedtak/log/metrics/MetricsUtil.java
+++ felles/log/src/main/java/no/nav/vedtak/log/metrics/MetricsUtil.java
@@ -7,9 +7,11 @@ import io.micrometer.core.instrument.Meter.Id;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmInfoMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
@@ -18,12 +20,15 @@ public class MetricsUtil {
     public static final PrometheusMeterRegistry REGISTRY = new PrometheusMeterRegistry(DEFAULT);
 
     static {
+
         Metrics.addRegistry(REGISTRY);
         new ClassLoaderMetrics().bindTo(globalRegistry);
         new JvmMemoryMetrics().bindTo(globalRegistry);
         new JvmGcMetrics().bindTo(globalRegistry);
         new ProcessorMetrics().bindTo(globalRegistry);
         new JvmThreadMetrics().bindTo(globalRegistry);
+        new JvmInfoMetrics().bindTo(globalRegistry);
+        new UptimeMetrics().bindTo(globalRegistry);
     }
 
     public static String scrape() {
