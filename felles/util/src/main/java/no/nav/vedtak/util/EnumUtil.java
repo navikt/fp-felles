e4d12788ffb63a38d339a911842fb2edff042865
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-10 11:03:57 +0200

    mer enum utils

--- felles/util/src/main/java/no/nav/vedtak/util/EnumUtil.java
+++ felles/util/src/main/java/no/nav/vedtak/util/EnumUtil.java
@@ -1,7 +1,9 @@
 package no.nav.vedtak.util;
 
 import java.util.EnumSet;
+import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public final class EnumUtil {
 
@@ -9,17 +11,27 @@ public final class EnumUtil {
 
     }
 
+    public static <E extends Enum<E>> E matchOrThrow(Class<E> type, Predicate<? super E> p) {
+        return matchOrThrow(type, p, () -> new IllegalArgumentException("fant ingen match for predikat " + p));
+    }
+
+    public static <E extends Enum<E>> E matchOrThrow(Class<E> type, Predicate<? super E> p, Supplier<? extends RuntimeException> exceptionSupplier) {
+        return find(type, p).orElseThrow(exceptionSupplier);
+    }
+
     public static <E extends Enum<E>> E match(Class<E> type, Predicate<? super E> p) {
-        return match(type, p, null);
+        return match(type, p, (E) null);
     }
 
     public static <E extends Enum<E>> E match(Class<E> type, Predicate<? super E> p, E defaultValue) {
+        return find(type, p)
+                .orElse(defaultValue);
+    }
+
+    private static <E extends Enum<E>> Optional<E> find(Class<E> type, Predicate<? super E> p) {
         return EnumSet.allOf(type)
                 .stream()
                 .filter(p)
-                .findFirst()
-                .orElse(defaultValue);
-
+                .findFirst();
     }
-
 }
