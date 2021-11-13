1a2bed1fe7e285c59106ef5a971036571b3b23eb
Michal J. Sladek <mrsladek@users.noreply.github.com>
2021-11-13 01:12:08 +0100

    Del 1. Forenklet IdProvider oppsett og lagt til rette for well-known … (#952)
    * Del 1. Forenklet IdProvider oppsett og lagt til rette for well-known discovery autokonfigurasjon.
    
    * Forenklet prop navn.
    
    * Del 2. legger inn azure validator konfig.

--- felles/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/oidc/OpenIDProviderConfigProvider.java
+++ felles/sikkerhet/src/main/java/no/nav/vedtak/sikkerhet/oidc/OpenIDProviderConfigProvider.java
@@ -15,110 +15,138 @@ import no.nav.vedtak.isso.OpenAMHelper;
 
 class OpenIDProviderConfigProvider {
     private static final Environment ENV = Environment.current();
-    private static final String LOGINSERVICE_IDPORTEN_DISCOVERY_URL = "loginservice.idporten.discovery.url";
-    private static final String LOGINSERVICE_IDPORTEN_AUDIENCE = "loginservice.idporten.audience";
 
-    private static final String TOKEN_X_WELL_KNOWN_URL = "token.x.well.known.url";
+    private static final String OPEN_AM_WELL_KNOWN_URL = ENV.getProperty("open.am.well.known.url");
+    private static final String OPEN_AM_CLIENT_ID = "open.am.client.id";
+
+    private static final String GANDALF_STS_WELL_KNOWN_URL = ENV.getProperty("gandalf.sts.well.known.url");
+
+    private static final String LOGINSERVICE_IDPORTEN_DISCOVERY_URL = ENV.getProperty("loginservice.idporten.discovery.url");
+    private static final String LOGINSERVICE_CLIENT_ID = "loginservice.idporten.audience";
+
+    private static final String AZURE_WELL_KNOWS_URL = ENV.getProperty("azure.app.well.known.url");
+    private static final String AZURE_CLIENT_ID = "azure.app.client.id";
+
+    private static final String TOKEN_X_WELL_KNOWN_URL = ENV.getProperty("token.x.well.known.url");
     private static final String TOKEN_X_CLIENT_ID = "token.x.client.id";
 
     public Set<OpenIDProviderConfig> getConfigs() {
         Set<OpenIDProviderConfig> configs = new HashSet<>();
-        configs.add(createOpenAmConfiguration(false, 30, true));
-        configs.add(
-                stsConfiguration(OidcTokenValidatorProvider.PROVIDERNAME_STS, false, 30, true));
-        configs.add(createOIDCConfiguration(OidcTokenValidatorProvider.PROVIDERNAME_AAD_B2C, !ENV.isLocal(), 30, false));
-        if (ENV.getProperty(TOKEN_X_WELL_KNOWN_URL) != null) {
-            configs.add(createTokenXConfiguration(ENV.getProperty(TOKEN_X_WELL_KNOWN_URL)));
+
+        // OpenAM
+        if (OPEN_AM_WELL_KNOWN_URL != null) {
+            configs.add(createOpenAmConfiguration(OPEN_AM_WELL_KNOWN_URL));
+        } else { // fallback til gammel måte
+            configs.add(createOpenAmConfiguration());
         }
-        configs.remove(null); // Fjerner en eventuell feilet konfigurasjon WTF ?
+
+        // Gandalf STS
+        if (GANDALF_STS_WELL_KNOWN_URL != null) {
+            configs.add(createGandalfStsConfiguration(GANDALF_STS_WELL_KNOWN_URL));
+        } else {
+            configs.add(createStsConfiguration());
+        }
+
+        // Azure
+        if (AZURE_WELL_KNOWS_URL != null) {
+            configs.add(createAzureAppConfiguration(AZURE_WELL_KNOWS_URL));
+        }
+
+        // Loginservice
+        if (LOGINSERVICE_IDPORTEN_DISCOVERY_URL != null) {
+            configs.add(createLoginServiceConfiguration(LOGINSERVICE_IDPORTEN_DISCOVERY_URL));
+        }
+
+        // TokenX
+        if (TOKEN_X_WELL_KNOWN_URL != null) {
+            configs.add(createTokenXConfiguration(TOKEN_X_WELL_KNOWN_URL));
+        }
+
         return configs;
     }
 
     /**
-     * For bakoverkompabilitet for eksisterende måte å konfigurere opp OIDC Vil
-     * benytte ny konfigurasjonsmåte hvis definert
+     * For bakoverkompabilitet for eksisterende måte å konfigurere opp OIDC mot openAm
      */
-    private OpenIDProviderConfig createOpenAmConfiguration(boolean useProxyForJwks, int allowedClockSkewInSeconds, boolean skipAudienceValidation) {
-        String providerName = OidcTokenValidatorProvider.PROVIDERNAME_OPEN_AM;
-        String clientName = ENV.getProperty(providerName + OidcTokenValidatorProvider.AGENT_NAME_KEY);
-        if (clientName != null) {
-            return createConfiguration(providerName, useProxyForJwks, allowedClockSkewInSeconds, skipAudienceValidation);
-        }
-
-        clientName = OpenAMHelper.getIssoUserName();
-        String issuer = OpenAMHelper.getIssoIssuerUrl();
-        String jwks = OpenAMHelper.getIssoJwksUrl();
-        return createConfiguration(providerName, issuer, jwks, useProxyForJwks, clientName, allowedClockSkewInSeconds,
-                skipAudienceValidation);
+    private OpenIDProviderConfig createOpenAmConfiguration() {
+        var clientName = OpenAMHelper.getIssoUserName();
+        var  issuer = OpenAMHelper.getIssoIssuerUrl();
+        var  jwks = OpenAMHelper.getIssoJwksUrl();
+        return createConfiguration("openam", issuer, jwks, false, clientName,
+            true);
     }
 
-    private OpenIDProviderConfig stsConfiguration(String providerName, boolean useProxyForJwks, int allowedClockSkewInSeconds,
-            boolean skipAudienceValidation) {
-        String issuer = ENV.getProperty(providerName + OidcTokenValidatorProvider.ALT_ISSUER_URL_KEY);
-        if (null == issuer) {
-            return null;
-        }
-        String clientName = "Client name is not used for STS";
-        String jwks = ENV.getProperty(providerName + OidcTokenValidatorProvider.ALT_JWKS_URL_KEY);
-        return createConfiguration(providerName, issuer, jwks, useProxyForJwks, clientName, allowedClockSkewInSeconds,
-                skipAudienceValidation);
+    /**
+     * For bakoverkompabilitet for eksisterende måte å konfigurere opp OIDC mot gandalf sts
+     */
+    private OpenIDProviderConfig createStsConfiguration() {
+        String clientName = "Client name is not used for Gandalf STS";
+        String issuer = ENV.getProperty("oidc.sts.issuer.url");
+        String jwks = ENV.getProperty("oidc.sts.jwks.url");
+        return createConfiguration("gandalfsts", issuer, jwks, false, clientName,
+            true);
+    }
+
+    private OpenIDProviderConfig createOpenAmConfiguration(String wellKnownUrl) {
+        return createConfiguration("openam",
+            getIssuerFra(wellKnownUrl),
+            getJwksFra(wellKnownUrl),
+            false,
+            ENV.getRequiredProperty(OPEN_AM_CLIENT_ID),
+            true);
+    }
+
+    private OpenIDProviderConfig createGandalfStsConfiguration(String wellKnownUrl) {
+        return createConfiguration("gandalfsts",
+            getIssuerFra(wellKnownUrl),
+            getJwksFra(wellKnownUrl),
+            false,
+            "Client name is not used for Gandalf STS",
+            true);
+    }
+
+    private OpenIDProviderConfig createAzureAppConfiguration(final String wellKnownUrl) {
+        return createConfiguration("azure",
+            getIssuerFra(wellKnownUrl),
+            getJwksFra(wellKnownUrl),
+            !ENV.isLocal(),
+            ENV.getRequiredProperty(AZURE_CLIENT_ID),
+            true);
+    }
+
+    private OpenIDProviderConfig createLoginServiceConfiguration(String wellKnownUrl) {
+        return createConfiguration("loginservice",
+            getIssuerFra(wellKnownUrl),
+            getJwksFra(wellKnownUrl),
+            !ENV.isLocal(),
+            ENV.getRequiredProperty(LOGINSERVICE_CLIENT_ID),
+            false);
     }
 
     private OpenIDProviderConfig createTokenXConfiguration(String wellKnownUrl) {
         return createConfiguration("tokenx",
-                getIssuerFra(wellKnownUrl),
-                getJwksFra(wellKnownUrl),
-                false,
-                ENV.getRequiredProperty(TOKEN_X_CLIENT_ID),
-                30,
-                false);
+            getIssuerFra(wellKnownUrl),
+            getJwksFra(wellKnownUrl),
+            false,
+            ENV.getRequiredProperty(TOKEN_X_CLIENT_ID),
+            false);
     }
 
-    private OpenIDProviderConfig createConfiguration(String providerName, boolean useProxyForJwks, int allowedClockSkewInSeconds,
-            boolean skipAudienceValidation) {
-        String clientName = ENV.getProperty(providerName + OidcTokenValidatorProvider.AGENT_NAME_KEY);
-        String issuer = ENV.getProperty(providerName + OidcTokenValidatorProvider.ISSUER_URL_KEY);
-        String jwks = ENV.getProperty(providerName + OidcTokenValidatorProvider.JWKS_URL_KEY);
-        return createConfiguration(providerName, issuer, jwks, useProxyForJwks, clientName, allowedClockSkewInSeconds,
-                skipAudienceValidation);
-    }
-
-    private OpenIDProviderConfig createOIDCConfiguration(String providerName, boolean useProxyForJwks, int allowedClockSkewInSeconds,
-            boolean skipAudienceValidation) {
-        return createConfiguration(providerName, issuer(providerName), jwks(providerName), useProxyForJwks, clientName(providerName),
-                allowedClockSkewInSeconds,
-                skipAudienceValidation);
-    }
-
-    private static String clientName(String providerName) {
-        return Optional.ofNullable(ENV.getProperty(LOGINSERVICE_IDPORTEN_AUDIENCE))
-                .orElse(ENV.getProperty(providerName + OidcTokenValidatorProvider.AGENT_NAME_KEY));
-    }
-
-    private static String jwks(String providerName) {
-        return Optional.ofNullable(ENV.getProperty(LOGINSERVICE_IDPORTEN_DISCOVERY_URL))
-                .map(OpenAMHelper::getJwksFra)
-                .orElse(ENV.getProperty(providerName + OidcTokenValidatorProvider.JWKS_URL_KEY));
-    }
-
-    private static String issuer(String providerName) {
-        return Optional.ofNullable(ENV.getProperty(LOGINSERVICE_IDPORTEN_DISCOVERY_URL))
-                .map(OpenAMHelper::getIssuerFra)
-                .orElse(ENV.getProperty(providerName + OidcTokenValidatorProvider.ISSUER_URL_KEY));
-    }
-
-    private OpenIDProviderConfig createConfiguration(String providerName, String issuer, String jwks, boolean useProxyForJwks, String clientName,
-            int allowedClockSkewInSeconds, boolean skipAudienceValidation) {
+    private OpenIDProviderConfig createConfiguration(String providerName,
+                                                     String issuer,
+                                                     String jwks,
+                                                     boolean useProxyForJwks,
+                                                     String clientName,
+                                                     boolean skipAudienceValidation) {
         return Optional.ofNullable(clientName)
                 .map(c -> new OpenIDProviderConfig(
                         url(issuer, "issuer", providerName),
                         url(jwks, "jwks", providerName),
                         useProxyForJwks,
                         c,
-                        allowedClockSkewInSeconds,
+                    30,
                         skipAudienceValidation))
-                .orElse(null);
-
+                .orElseThrow();
     }
 
     private static URL url(String url, String key, String providerName) {
@@ -126,8 +154,7 @@ class OpenIDProviderConfigProvider {
             return new URL(url);
         } catch (MalformedURLException e) {
             throw new TekniskException("F-644196",
-                    String.format("Syntaksfeil i OIDC konfigurasjonen av '%s' for '%s'", key, providerName), e);
+                    String.format("Syntaksfeil i token validator konfigurasjonen av '%s' for '%s'", key, providerName), e);
         }
     }
-
 }
