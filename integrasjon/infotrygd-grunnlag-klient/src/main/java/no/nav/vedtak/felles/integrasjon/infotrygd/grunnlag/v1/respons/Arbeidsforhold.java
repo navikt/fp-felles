c9f11745cb3b3aac9163b4d1a59c6526e074f7b9
Espen Velsvik <espenjv@users.noreply.github.com>
2022-03-15 12:38:56 +0100

    Legger til felt for refusjon-tom (#1103)

--- integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Arbeidsforhold.java
+++ integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Arbeidsforhold.java
@@ -1,13 +1,16 @@
 package no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons;
 
+import java.time.LocalDate;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 
 public record Arbeidsforhold(
-        @JsonAlias("arbeidsgiverOrgnr") Orgnummer orgnr,
-        @JsonAlias("inntektForPerioden") Integer inntekt,
-        Inntektsperiode inntektsperiode,
-        Boolean refusjon) {
+    @JsonAlias("arbeidsgiverOrgnr") Orgnummer orgnr,
+    @JsonAlias("inntektForPerioden") Integer inntekt,
+    Inntektsperiode inntektsperiode,
+    Boolean refusjon,
+    LocalDate refusjonTom) {
 
 
     @Deprecated(since = "4.0.x", forRemoval = true)
