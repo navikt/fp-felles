046ed794c2b71a1eebaafebf1cb39615bf15a7b5
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-03-30 12:20:20 +0200

    Legger til felter for orgnr, dagsats og erRefusjon i infotrygdvedtak

--- integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Vedtak.java
+++ integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Vedtak.java
@@ -1,6 +1,6 @@
 package no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons;
 
-public record Vedtak(Periode periode, int utbetalingsgrad) {
+public record Vedtak(Periode periode, int utbetalingsgrad, String arbeidsgiverOrgnr, Boolean erRefusjon, Integer dagsats) {
 
     @Deprecated(since = "4.0.x", forRemoval = true)
     public Periode getPeriode() {
