06171f0c0e9e09bb7b1085c7853f3ab109882f07
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-10-04 18:37:47 +0200

    Unngå både bean og producer for Persondata/PdlKlient

--- integrasjon/pdl-klient/src/main/java/no/nav/vedtak/felles/integrasjon/pdl/RestSupportClientProducer.java
+++ integrasjon/pdl-klient/src/main/java/no/nav/vedtak/felles/integrasjon/pdl/RestSupportClientProducer.java
@@ -3,17 +3,11 @@ package no.nav.vedtak.felles.integrasjon.pdl;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import no.nav.vedtak.felles.integrasjon.person.Persondata;
 import no.nav.vedtak.felles.integrasjon.rest.RestClient;
 import no.nav.vedtak.klient.http.DefaultHttpClient;
 
 @ApplicationScoped
 public class RestSupportClientProducer {
-
-    @Produces
-    public Persondata getPdlClient() {
-        return new PdlKlient();
-    }
 
     @Produces
     public RestClient getRestClient() {
