242284ccb8aa4867e83e1aaea76808bdd33c2ef9
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-10-12 15:33:25 +0200

    Revert recordifisering av IT Grunnlag (#1199)

--- integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Orgnummer.java
+++ integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Orgnummer.java
@@ -1,11 +1,47 @@
 package no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Objects;
 
-public record Orgnummer(String orgnr) {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class Orgnummer {
+
+    private final String orgnr;
 
     @JsonCreator
     public static Orgnummer forValue(String orgnr) {
         return new Orgnummer(orgnr);
+    }
+
+    private Orgnummer(String orgnr) {
+        this.orgnr = orgnr;
+    }
+
+    @JsonValue
+    public String getOrgnr() {
+        return orgnr;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orgnr);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Orgnummer)) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        Orgnummer that = (Orgnummer) obj;
+        return Objects.equals(that.orgnr, this.orgnr);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[orgnr=" + orgnr + "]";
     }
 }
