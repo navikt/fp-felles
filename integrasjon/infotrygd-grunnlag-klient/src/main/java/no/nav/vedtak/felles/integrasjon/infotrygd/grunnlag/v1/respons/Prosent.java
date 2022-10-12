9eb8b15d5b82430017af93f310631860c078b532
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-10-12 15:09:44 +0200

    Revert recordifisering av IT Grunnlag

--- integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Prosent.java
+++ integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Prosent.java
@@ -1,11 +1,47 @@
 package no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Objects;
 
-public record Prosent(Integer prosent) {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class Prosent {
+
+    private final Integer prosent;
 
     @JsonCreator
     public static Prosent forValue(Integer value) {
         return new Prosent(value);
+    }
+
+    private Prosent(Integer prosent) {
+        this.prosent = prosent;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prosent);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Prosent)) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        Prosent that = (Prosent) obj;
+        return Objects.equals(that.prosent, this.prosent);
+    }
+
+    @JsonValue
+    public Integer getProsent() {
+        return prosent;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [prosent=" + prosent + "]";
     }
 }
