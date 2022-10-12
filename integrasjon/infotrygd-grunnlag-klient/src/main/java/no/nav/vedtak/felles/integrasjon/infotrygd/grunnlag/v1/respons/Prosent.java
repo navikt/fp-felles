3d489c157fc58d905f37354ecabd0caa0a8e25a6
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-10-12 22:16:18 +0200

    Infotrygt grunnlag as record, jsonvalues, test-sample

--- integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Prosent.java
+++ integrasjon/infotrygd-grunnlag-klient/src/main/java/no/nav/vedtak/felles/integrasjon/infotrygd/grunnlag/v1/respons/Prosent.java
@@ -1,47 +1,17 @@
 package no.nav.vedtak.felles.integrasjon.infotrygd.grunnlag.v1.respons;
 
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-public class Prosent {
-
-    private final Integer prosent;
+public record Prosent(Integer prosent) {
 
     @JsonCreator
     public static Prosent forValue(Integer value) {
         return new Prosent(value);
     }
 
-    private Prosent(Integer prosent) {
-        this.prosent = prosent;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(prosent);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null || !(obj instanceof Prosent)) {
-            return false;
-        }
-        if (this == obj) {
-            return true;
-        }
-        Prosent that = (Prosent) obj;
-        return Objects.equals(that.prosent, this.prosent);
-    }
-
     @JsonValue
-    public Integer getProsent() {
+    public Integer prosent() {
         return prosent;
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + " [prosent=" + prosent + "]";
     }
 }
