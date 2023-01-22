8823f2e9ef9017d28fbe60698eb9c7c25a82dcfe
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-01-22 19:37:55 +0100

    Deserialiser med forventet offsetdatetime (#1237)

---
+++ integrasjon/oppgave-rest-klient/src/test/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/OppgaveRestTest.java
@@ -0,0 +1,46 @@
+package no.nav.vedtak.felles.integrasjon.oppgave.v1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
+
+class OppgaveRestTest {
+
+    private static final ObjectWriter WRITER = DefaultJsonMapper.getObjectMapper().writerWithDefaultPrettyPrinter();
+    private static final ObjectReader READER = DefaultJsonMapper.getObjectMapper().reader();
+
+    private static final String json = """
+        {
+          "id": 357736794,
+          "tildeltEnhetsnr":"1234",
+          "opprettetAvEnhetsnr":"1234",
+          "saksreferanse":"152200771",
+          "aktoerId":"1000000000000",
+          "beskrivelse":"Må behandle sak i VL!",
+          "temagruppe":"FMLI",
+          "tema":"FOR",
+          "behandlingstema":"ab0326",
+          "oppgavetype":"BEH_SAK_VL",
+          "versjon":1,
+          "opprettetAv":"srvengangsstonad",
+          "prioritet":"NORM",
+          "status": "OPPRETTET",
+          "metadata":{},
+          "fristFerdigstillelse":"2023-01-23",
+          "aktivDato":"2023-01-22",
+          "opprettetTidspunkt":"2023-01-22T18:52:45.206+01:00"
+        }
+        """;
+
+    @Test
+    void test_response() throws Exception {
+        var deserialized = DefaultJsonMapper.fromJson(json, Oppgave.class);
+        assertThat(deserialized).isNotNull();
+        assertThat(deserialized.tildeltEnhetsnr()).isEqualTo("1234");
+    }
+}
