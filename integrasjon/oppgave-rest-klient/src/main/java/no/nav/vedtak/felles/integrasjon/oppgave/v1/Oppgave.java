ee452d7130d6b11daee6e188533f0dea94d3b464
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-01-22 19:34:00 +0100

    Deserialiser med forventet offsetdatetime

--- integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/Oppgave.java
+++ integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/Oppgave.java
@@ -2,8 +2,11 @@ package no.nav.vedtak.felles.integrasjon.oppgave.v1;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Oppgave(Long id,
@@ -22,5 +25,12 @@ public record Oppgave(Long id,
                       Prioritet prioritet,
                       Oppgavestatus status,
                       String beskrivelse,
-                      LocalDateTime opprettetTidspunkt) {
+                      @JsonDeserialize(using = LocalOffsetDateTimeFormatter.class) LocalDateTime opprettetTidspunkt) {
+
+    private static class LocalOffsetDateTimeFormatter extends LocalDateTimeDeserializer {
+        public LocalOffsetDateTimeFormatter() {
+            super(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+    }
+
 }
