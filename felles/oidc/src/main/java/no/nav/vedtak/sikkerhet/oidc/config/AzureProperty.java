b819340f1cf02f25d110c489dacfac48e1ce7eeb
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-02-22 14:08:51 +0100

    Støtte lokale lowercase nais-props (#1251)

--- felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/config/AzureProperty.java
+++ felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/config/AzureProperty.java
@@ -15,12 +15,6 @@ public enum AzureProperty {
     AZURE_OPENID_CONFIG_JWKS_URI,
     AZURE_OPENID_CONFIG_TOKEN_ENDPOINT
     ;
-    //     public  static final String AZURE_WELL_KNOWN_URL = "azure.app.well.known.url"; // naiserator
-    //    public  static final String AZURE_CONFIG_ISSUER = "azure.openid.config.issuer"; // naiserator
-    //    public  static final String AZURE_CONFIG_JWKS_URI = "azure.openid.config.jwks.uri"; // naiserator
-    //    private static final String AZURE_CONFIG_TOKEN_ENDPOINT = "azure.openid.config.token.endpoint"; // naiserator
-    //    public  static final String AZURE_CLIENT_ID = "azure.app.client.id"; // naiserator
-    //    private static final String AZURE_CLIENT_SECRET = "azure.app.client.secret"; // naiserator
 
     public static final String NAV_IDENT = "NAVident";
     public static final String AZP_NAME = "azp_name";
