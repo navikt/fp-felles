658c30ef28a7c9f1ac662cc2487659a52a6f7628
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-02-25 14:06:15 +0100

    Deprecations

--- integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/Oppgaver.java
+++ integrasjon/oppgave-rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/oppgave/v1/Oppgaver.java
@@ -12,12 +12,14 @@ public interface Oppgaver {
 
     void ferdigstillOppgave(String oppgaveId);
 
+    @Deprecated(forRemoval = true)
     List<Oppgave> finnÅpneOppgaver(String aktørId, String tema, List<String> oppgaveTyper);
 
     // Henter åpne oppgaver med Tema-kode Foreldrepenger for gitt Oppgavetyper.
     // Øvrige argumenter kan være null,  men hvis det ventes mange oppgaver så sett aktør, enhet eller limit
     List<Oppgave> finnÅpneOppgaverAvType(Oppgavetype oppgaveType, String aktørId, String enhetsNr, String limit);
 
+    @Deprecated(forRemoval = true)
     List<Oppgave> finnÅpneOppgaverForEnhet(String tema, List<String> oppgaveTyper, String enhetsNr, String limit);
 
     // Henter åpne oppgaver med Tema-kode Foreldrepenger for gitte Opgavetyper-kodeverk (kan være tom).
