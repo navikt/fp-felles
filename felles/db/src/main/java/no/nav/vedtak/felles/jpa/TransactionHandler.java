8cdb169f923213cf7bacc6915ad464bfe483aa32
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-08 15:20:55 +0100

    Kommenter et par false positives

--- felles/db/src/main/java/no/nav/vedtak/felles/jpa/TransactionHandler.java
+++ felles/db/src/main/java/no/nav/vedtak/felles/jpa/TransactionHandler.java
@@ -37,10 +37,10 @@ public abstract class TransactionHandler<R> {
         }
     }
 
-    protected abstract R doWork(EntityManager entityManager) throws Exception;
+    protected abstract R doWork(EntityManager entityManager) throws Exception; // NOSONAR
 
     public interface Work<R> {
-        R doWork(EntityManager em) throws Exception;
+        R doWork(EntityManager em) throws Exception;  // NOSONAR
     }
 
 }
