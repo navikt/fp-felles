5cb6be066d13f5228f309ba2bde10f563db53e80
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-08-22 21:05:04 +0200

    Et par kommentarer

--- integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/TokenFlow.java
+++ integrasjon/rest-klient/src/main/java/no/nav/vedtak/felles/integrasjon/rest/TokenFlow.java
@@ -2,9 +2,9 @@ package no.nav.vedtak.felles.integrasjon.rest;
 
 public enum TokenFlow {
     ADAPTIVE, // DWIM for targets accepting both azuread, sts tokens, and tokenx. STS->AzureCC
-    STS_CC,
+    STS_CC, // Mot endepunkt som insisterer på STS og ikke aksepterer Azure
     STS_ADD_CONSUMER, // Trengs inntil videre pga brreg.proxy
-    AZUREAD_CC,
+    AZUREAD_CC, // Mot endepunkt som bare støtter AzureCC, ikke AzureOBO-flow
     NO_AUTH_NEEDED;
 
     // Does the endpoint require an Azure AD token?
