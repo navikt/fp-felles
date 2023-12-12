c1fad50fc6806a8d5ef2a3164c2364d8b213686a
Michal J. Sladek <michal.sladek@nav.no>
2023-12-12 16:02:19 +0100

    Fikser sonar

--- felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/validator/ConsumerMetric.java
+++ felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/validator/ConsumerMetric.java
@@ -3,28 +3,26 @@ package no.nav.vedtak.sikkerhet.oidc.validator;
 import static no.nav.vedtak.log.metrics.MetricsUtil.REGISTRY;
 
 import io.micrometer.core.instrument.Counter;
-import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.sikkerhet.kontekst.IdentType;
 import no.nav.vedtak.sikkerhet.oidc.config.OpenIDProvider;
 
 public class ConsumerMetric {
 
-    private static final Environment ENV = Environment.current();
     private static final String FORELDREPENGER_KONSUMENTER = "foreldrepenger.konsumenter";
 
     private ConsumerMetric() {
     }
 
-    public static void registrer(String clientName, String konsument, OpenIDProvider tokenType, IdentType identType, String acrLevel) {
-        counter(clientName, konsument, tokenType, identType, acrLevel).increment();
+    public static void registrer(String klientNavn, String konsument, OpenIDProvider tokenType, IdentType identType, String acrLevel) {
+        counter(klientNavn, konsument, tokenType, identType, acrLevel).increment();
     }
-    public static void registrer(String clientName, String konsument, OpenIDProvider tokenType, IdentType identType) {
-        counter(clientName, konsument, tokenType, identType, null).increment();
+    public static void registrer(String klientNavn, String konsument, OpenIDProvider tokenType, IdentType identType) {
+        counter(klientNavn, konsument, tokenType, identType, null).increment();
     }
 
-    private static Counter counter(String clientName, String konsument, OpenIDProvider tokenType, IdentType identType, String acrLevel) {
+    private static Counter counter(String klientNavn, String konsument, OpenIDProvider tokenType, IdentType identType, String acrLevel) {
         var counter = Counter.builder(FORELDREPENGER_KONSUMENTER)
-            .tag("klient", clientName)
+            .tag("klient", klientNavn)
             .tag("tokenType", tokenType.name())
             .tag("identYype", identType.name())
             .tag("konsument", konsument)
