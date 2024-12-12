f5c58a12dc941f1cd14c72bcf2ba6d2cc7d8c8b8
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-12-12 15:42:13 +0100

    refactor: fjerner ubrukte interface som er flyttet til felles-server (#1407)

--- felles/log/src/main/java/no/nav/vedtak/log/metrics/LiveAndReadinessAware.java
+++
@@ -1,9 +0,0 @@
-package no.nav.vedtak.log.metrics;
-
-@Deprecated(forRemoval = true)
-/*
- * @deprecated see no.nav.vedtak.server
- */
-public interface LiveAndReadinessAware extends LivenessAware, ReadinessAware {
-
-}
