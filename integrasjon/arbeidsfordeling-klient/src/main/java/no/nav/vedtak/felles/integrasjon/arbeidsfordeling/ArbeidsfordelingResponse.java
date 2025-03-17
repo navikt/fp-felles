2f3bc1dfb9034782f630c9c69dee7df7bdb2f505
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-03-17 12:22:08 +0100

    TFP-6043 sanere norg2-klient (#1422)

--- integrasjon/arbeidsfordeling-klient/src/main/java/no/nav/vedtak/felles/integrasjon/arbeidsfordeling/ArbeidsfordelingResponse.java
+++
@@ -1,7 +0,0 @@
-package no.nav.vedtak.felles.integrasjon.arbeidsfordeling;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ArbeidsfordelingResponse(@JsonProperty("enhetNr") String enhetNr, @JsonProperty("navn") String enhetNavn, String status,
-                                       @JsonProperty("type") String enhetType) {
-}
