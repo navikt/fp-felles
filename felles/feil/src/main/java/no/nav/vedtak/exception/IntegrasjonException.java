95d89cf9313f0624af8ad159b6729f9dd6fa9e6b
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-04-09 21:10:56 +0200

    TFP-5972 returkonvensjon PDL-PIP (#1430)

--- felles/feil/src/main/java/no/nav/vedtak/exception/IntegrasjonException.java
+++ felles/feil/src/main/java/no/nav/vedtak/exception/IntegrasjonException.java
@@ -2,11 +2,26 @@ package no.nav.vedtak.exception;
 
 public class IntegrasjonException extends VLException {
 
+    private final Integer statusCode;
+
     public IntegrasjonException(String kode, String msg) {
-        this(kode, msg, null);
+        this(kode, msg, null, null);
     }
 
     public IntegrasjonException(String kode, String msg, Throwable cause) {
+        this(kode, msg, null, cause);
+    }
+
+    public IntegrasjonException(String kode, String msg, int statusCode) {
+        this(kode, msg, statusCode, null);
+    }
+
+    public IntegrasjonException(String kode, String msg, Integer statusCode, Throwable cause) {
         super(kode, msg, cause);
+        this.statusCode = statusCode;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
     }
 }
