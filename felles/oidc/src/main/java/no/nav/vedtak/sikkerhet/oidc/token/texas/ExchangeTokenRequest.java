c425242c7fd6b1dd751ac309e4e1c92c4fa4cc9a
Michal J. Sladek <michal.sladek@nav.no>
2026-03-05 09:40:46 +0100

    skip cache på token exchange

--- felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/token/texas/ExchangeTokenRequest.java
+++ felles/oidc/src/main/java/no/nav/vedtak/sikkerhet/oidc/token/texas/ExchangeTokenRequest.java
@@ -3,7 +3,7 @@ package no.nav.vedtak.sikkerhet.oidc.token.texas;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ExchangeTokenRequest(IdProvider identity_provider, String user_token, String target) {
+public record ExchangeTokenRequest(IdProvider identity_provider, String user_token, String target, Boolean skip_cache) {
 
     @Override
     public String toString() {
@@ -11,6 +11,7 @@ public record ExchangeTokenRequest(IdProvider identity_provider, String user_tok
             "identity_provider=" + identity_provider +
             ", user_token=<redacted>" +
             ", target='" + target +
+            (skip_cache != null ? ", skip_cache='" + skip_cache + '\'' : "") +
             '}';
     }
 }
